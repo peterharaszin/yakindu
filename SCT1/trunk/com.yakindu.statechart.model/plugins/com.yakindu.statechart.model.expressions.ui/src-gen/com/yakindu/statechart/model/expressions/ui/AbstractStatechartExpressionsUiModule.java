471c50d87927624e28c604899d5ea34d955003cb
terfloth@itemis.de <terfloth@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2010-06-20 00:30:20 +0000

    Nearly complete clean new set up expression language xtext projects

--- SCT1/trunk/com.yakindu.statechart.model/plugins/com.yakindu.statechart.model.expressions.ui/src-gen/com/yakindu/statechart/model/expressions/ui/AbstractStatechartExpressionsUiModule.java
+++ SCT1/trunk/com.yakindu.statechart.model/plugins/com.yakindu.statechart.model.expressions.ui/src-gen/com/yakindu/statechart/model/expressions/ui/AbstractStatechartExpressionsUiModule.java
@@ -48,6 +48,21 @@ public abstract class AbstractStatechartExpressionsUiModule extends DefaultUiMod
 		binder.bind(org.eclipse.xtext.parser.antlr.ITokenDefProvider.class).annotatedWith(com.google.inject.name.Names.named(org.eclipse.xtext.ui.LexerUIBindings.HIGHLIGHTING)).to(org.eclipse.xtext.parser.antlr.AntlrTokenDefProvider.class);
 	}
 
+	// contributed by org.eclipse.xtext.generator.exporting.QualifiedNamesFragment
+	public Class<? extends org.eclipse.xtext.ui.editor.contentassist.PrefixMatcher> bindPrefixMatcher() {
+		return org.eclipse.xtext.ui.editor.contentassist.FQNPrefixMatcher.class;
+	}
+
+	// contributed by org.eclipse.xtext.generator.builder.BuilderIntegrationFragment
+	public void configureIResourceDescriptionsBuilderScope(com.google.inject.Binder binder) {
+		binder.bind(org.eclipse.xtext.resource.IResourceDescriptions.class).annotatedWith(com.google.inject.name.Names.named(org.eclipse.xtext.scoping.impl.AbstractGlobalScopeProvider.NAMED_BUILDER_SCOPE)).to(org.eclipse.xtext.builder.builderState.ShadowingResourceDescriptions.class);
+	}
+
+	// contributed by org.eclipse.xtext.generator.builder.BuilderIntegrationFragment
+	public Class<? extends org.eclipse.xtext.ui.editor.IXtextEditorCallback> bindIXtextEditorCallback() {
+		return org.eclipse.xtext.builder.nature.NatureAddingEditorCallback.class;
+	}
+
 	// contributed by org.eclipse.xtext.ui.generator.labeling.LabelProviderFragment
 	public Class<? extends org.eclipse.jface.viewers.ILabelProvider> bindILabelProvider() {
 		return com.yakindu.statechart.model.expressions.ui.labeling.StatechartExpressionsLabelProvider.class;
@@ -66,6 +81,11 @@ public abstract class AbstractStatechartExpressionsUiModule extends DefaultUiMod
 	// contributed by org.eclipse.xtext.ui.generator.outline.OutlineNodeAdapterFactoryFragment
 	public Class<? extends org.eclipse.xtext.ui.editor.outline.actions.IContentOutlineNodeAdapterFactory> bindIContentOutlineNodeAdapterFactory() {
 		return com.yakindu.statechart.model.expressions.ui.outline.StatechartExpressionsOutlineNodeAdapterFactory.class;
+	}
+
+	// contributed by org.eclipse.xtext.ui.generator.quickfix.QuickfixProviderFragment
+	public Class<? extends org.eclipse.xtext.ui.editor.quickfix.IssueResolutionProvider> bindIssueResolutionProvider() {
+		return com.yakindu.statechart.model.expressions.ui.quickfix.StatechartExpressionsQuickfixProvider.class;
 	}
 
 	// contributed by org.eclipse.xtext.ui.generator.contentAssist.JavaBasedContentAssistFragment

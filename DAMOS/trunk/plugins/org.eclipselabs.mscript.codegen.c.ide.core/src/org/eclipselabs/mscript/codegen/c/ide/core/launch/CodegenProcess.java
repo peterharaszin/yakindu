b2666ab16739320a2c96d9a1b5e15b7ad07f2f20
au@andreasunger.net <au@andreasunger.net@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2010-12-07 20:54:26 +0000

    CodegenProcess added.

---
+++ DAMOS/trunk/plugins/org.eclipselabs.mscript.codegen.c.ide.core/src/org/eclipselabs/mscript/codegen/c/ide/core/launch/CodegenProcess.java
@@ -0,0 +1,111 @@
+/****************************************************************************
+ * Copyright (c) 2008, 2009 Andreas Unger and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andreas Unger - initial API and implementation 
+ ****************************************************************************/
+
+package org.eclipselabs.mscript.codegen.c.ide.core.launch;
+
+import org.eclipse.debug.core.DebugEvent;
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.model.IProcess;
+import org.eclipse.debug.core.model.IStreamsProxy;
+
+/**
+ * @author Andreas Unger
+ *
+ */
+public class CodegenProcess implements IProcess {
+
+	private ILaunch launch;
+	private String name;
+
+	private boolean terminated;
+
+	/**
+	 * 
+	 */
+	public CodegenProcess(ILaunch launch, String name) {
+		this.launch = launch;
+		this.name = name;
+		launch.addProcess(this);
+		fireCreationEvent();
+	}
+
+	public ILaunch getLaunch() {
+		return launch;
+	}
+	
+	public String getLabel() {
+		return name;
+	}
+	
+	public void run() {
+		terminated = true;
+		fireTerminateEvent();
+	}
+
+	public boolean canTerminate() {
+		return !terminated;
+	}
+
+	public boolean isTerminated() {
+		return terminated;
+	}
+
+	public void terminate() throws DebugException {
+	}
+
+	public String getAttribute(String key) {
+		return null;
+	}
+
+	public void setAttribute(String key, String value) {
+	}
+
+	public IStreamsProxy getStreamsProxy() {
+		return null;
+	}
+
+	public int getExitValue() throws DebugException {
+		return 0;
+	}
+
+	/**
+	 * Fires a creation event.
+	 */
+	protected void fireCreationEvent() {
+		fireEvent(new DebugEvent(this, DebugEvent.CREATE));
+	}
+
+	/**
+	 * Fires a terminate event.
+	 */
+	protected void fireTerminateEvent() {
+		fireEvent(new DebugEvent(this, DebugEvent.TERMINATE));
+	}
+
+	/**
+	 * Fires the given debug event.
+	 * 
+	 * @param event debug event to fire
+	 */
+	protected void fireEvent(DebugEvent event) {
+		DebugPlugin manager= DebugPlugin.getDefault();
+		if (manager != null) {
+			manager.fireDebugEventSet(new DebugEvent[] { event });
+		}
+	}
+
+	public Object getAdapter(@SuppressWarnings("rawtypes") Class adapter) {
+		return null;
+	}
+
+}

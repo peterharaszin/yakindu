f0b58514da94df2ac0411e555207a6737835675a
au@andreasunger.net <au@andreasunger.net@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-04-20 18:44:28 +0000

    Connection junctions are now drawn.

--- DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/figures/ConnectionFigure.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/figures/ConnectionFigure.java
@@ -11,16 +11,87 @@
 
 package org.eclipselabs.damos.diagram.ui.figures;
 
+import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.PolygonDecoration;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.PolylineConnectionEx;
+import org.eclipse.gmf.runtime.draw2d.ui.geometry.PointListUtilities;
 
 public class ConnectionFigure extends PolylineConnectionEx implements IFigureConstants {
 	
+	private static final int JUNCTION_RADIUS = 80;
+	
+	private static final double DECORATION_SCALE_X = 182;
+	private static final double DECORATION_SCALE_Y = 78;
+	
 	public ConnectionFigure() {
 		PolygonDecoration decoration = new PolygonDecoration();
-		decoration.setScale(182, 78);
+		decoration.setScale(DECORATION_SCALE_X, DECORATION_SCALE_Y);
 		setTargetDecoration(decoration);
 		setLineWidth(DEFAULT_LINE_WIDTH);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.draw2d.Shape#paintFigure(org.eclipse.draw2d.Graphics)
+	 */
+	@Override
+	public void paintFigure(Graphics graphics) {
+		super.paintFigure(graphics);
+		if (hasJunction()) {
+			Point p = getPoints().getPoint(1);
+			graphics.setBackgroundColor(graphics.getForegroundColor());
+			graphics.fillOval(p.x - JUNCTION_RADIUS, p.y - JUNCTION_RADIUS, 2 * JUNCTION_RADIUS, 2 * JUNCTION_RADIUS);
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.gmf.runtime.draw2d.ui.figures.PolylineConnectionEx#getSmoothPoints(boolean)
+	 */
+	@Override
+	public PointList getSmoothPoints(boolean calculateAppox) {
+		PointList points = PointListUtilities.copyPoints(getPoints());
+		if (points.size() > 0 && hasJunction()) {
+			points.removePoint(0);
+		}
+		return points;
+	}
+	
+	private boolean hasJunction() {
+		boolean foundThis = false;
+		for (Object o : getParent().getChildren()) {
+			if (o == this) {
+				foundThis = true;
+				continue;
+			}
+			if (o instanceof ConnectionFigure) {
+				ConnectionFigure connectionFigure = (ConnectionFigure) o;
+				if (connectionFigure.getSourceAnchor().getOwner() == getSourceAnchor().getOwner()
+						&& connectionFigure.getStart().equals(getStart())) {
+					Point junctionPoint = getPoints().getPoint(1);
+					Point otherPoint1 = connectionFigure.getPoints().getPoint(0);
+					Point otherPoint2 = connectionFigure.getPoints().getPoint(1);
+					if (junctionPoint.x == otherPoint2.x) {
+						int otherY1 = Math.min(otherPoint1.y, otherPoint2.y);
+						int otherY2 = Math.max(otherPoint1.y, otherPoint2.y);
+						if (foundThis && (junctionPoint.y == otherY1 || junctionPoint.y == otherY2)) {
+							return true;
+						} else if(junctionPoint.y > otherY1 && junctionPoint.y < otherY2) {
+							return true;
+						}
+					} else if (junctionPoint.y == otherPoint2.y) {
+						int otherX1 = Math.min(otherPoint1.x, otherPoint2.x);
+						int otherX2 = Math.max(otherPoint1.x, otherPoint2.x);
+						if (foundThis && (junctionPoint.x == otherX1 && junctionPoint.x == otherX2)) {
+							return true;
+						} else if (junctionPoint.x > otherX1 && junctionPoint.x < otherX2) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
 	}
 	
 }

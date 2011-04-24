d1abf6624c73ab1d1d0191dd97ebf25bd2d62a07
au@andreasunger.net <au@andreasunger.net@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-04-24 08:41:11 +0000

    WhileLoopCondition movement handling improved.

--- DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/internal/editpolicies/CompoundConstrainedToolbarLayoutEditPolicy.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/internal/editpolicies/CompoundConstrainedToolbarLayoutEditPolicy.java
@@ -11,13 +11,21 @@
 
 package org.eclipselabs.damos.diagram.ui.internal.editpolicies;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPolicy;
+import org.eclipse.gef.GraphicalEditPart;
+import org.eclipse.gef.SharedCursors;
+import org.eclipse.gef.handles.NonResizableHandleKit;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.IBorderItemEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ResizableCompartmentEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.BorderItemSelectionEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ConstrainedToolbarLayoutEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ResizableCompartmentEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.tools.DragEditPartsTrackerEx;
 
 /**
  * @author Andreas Unger
@@ -27,15 +35,35 @@ public class CompoundConstrainedToolbarLayoutEditPolicy extends ConstrainedToolb
 
 	@Override
 	protected EditPolicy createChildEditPolicy(EditPart child) {
-		if (child.getEditPolicy(EditPolicy.PRIMARY_DRAG_ROLE) == null
-				&& child instanceof ResizableCompartmentEditPart) {
-			return new ResizableCompartmentEditPolicy(isHorizontal()) {
+		if (child.getEditPolicy(EditPolicy.PRIMARY_DRAG_ROLE) == null) {
+			if (child instanceof ResizableCompartmentEditPart) {
+				return new ResizableCompartmentEditPolicy(isHorizontal()) {
+	
+					protected java.util.List<?> createCollapseHandles() {
+						return Collections.emptyList();
+					}
+					
+				};
+			}
+			if (child instanceof IBorderItemEditPart) {
+				return new BorderItemSelectionEditPolicy() {
+					
+					@Override
+					protected List<?> createSelectionHandles() {
+						List<?> list = new ArrayList<Object>();
+						NonResizableHandleKit.addHandles((GraphicalEditPart) getHost(),
+								list, new DragEditPartsTrackerEx(getHost()) {
 
-				protected java.util.List<?> createCollapseHandles() {
-					return Collections.emptyList();
-				}
-				
-			};
+							protected boolean isMove() {
+								return true;
+							}
+							
+						}, SharedCursors.ARROW);
+						return list;
+					}
+										
+				};
+			}
 		}
 		return super.createChildEditPolicy(child);
 	}

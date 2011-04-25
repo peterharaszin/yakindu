5db80be5a504de1731cf9dec552a0f1ed7683809
au@andreasunger.net <au@andreasunger.net@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-04-25 10:26:34 +0000

    Preset action link conditions in ConfigureActionLinkCommand.

--- DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.core/src/org/eclipselabs/damos/diagram/core/internal/commands/ConfigureActionLinkCommand.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.core/src/org/eclipselabs/damos/diagram/core/internal/commands/ConfigureActionLinkCommand.java
@@ -11,6 +11,9 @@
 
 package org.eclipselabs.damos.diagram.core.internal.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -21,8 +24,14 @@ import org.eclipse.gmf.runtime.emf.type.core.requests.ConfigureRequest;
 import org.eclipse.gmf.runtime.emf.type.core.requests.CreateRelationshipRequest;
 import org.eclipselabs.damos.dml.Action;
 import org.eclipselabs.damos.dml.ActionLink;
+import org.eclipselabs.damos.dml.ConditionSpecification;
+import org.eclipselabs.damos.dml.DMLFactory;
+import org.eclipselabs.damos.dml.OpaqueConditionSpecification;
 
 public class ConfigureActionLinkCommand extends ConfigureElementCommand {
+	
+	private static final String TRUE = "true";
+	private static final String FALSE = "false";
 	
     public ConfigureActionLinkCommand(ConfigureRequest request, EClass configurableType) {
         super(request);
@@ -33,7 +42,37 @@ public class ConfigureActionLinkCommand extends ConfigureElementCommand {
 		ConfigureRequest request = (ConfigureRequest) getRequest();
     	ActionLink actionLink = (ActionLink) request.getElementToConfigure();
     	actionLink.setAction((Action) request.getParameter(CreateRelationshipRequest.TARGET));
+    	
+    	List<ActionLink> actionLinks = actionLink.getChoice().getActionLinks();
+    	if (actionLinks.size() < 3) {
+    		actionLinks = new ArrayList<ActionLink>(actionLinks);
+	    	actionLinks.remove(actionLink);
+	    	
+	    	switch (actionLinks.size()) {
+	    	case 0:
+	    		setActionLinkCondition(actionLink, TRUE);
+	    		break;
+	    	case 1:
+	    		ConditionSpecification conditionSpecification = actionLinks.get(0).getCondition();
+	    		if (conditionSpecification != null) {
+					String condition = conditionSpecification.stringCondition();
+					if (TRUE.equals(condition)) {
+		    			setActionLinkCondition(actionLink, FALSE);
+		    		} else if (FALSE.equals(condition)) {
+		    			setActionLinkCondition(actionLink, TRUE);
+		    		}
+	    		}
+	    		break;
+	    	}
+    	}
+    	
 		return CommandResult.newOKCommandResult(actionLink);
+	}
+	
+	private void setActionLinkCondition(ActionLink actionLink, String condition) {
+		OpaqueConditionSpecification conditionSpecification = DMLFactory.eINSTANCE.createOpaqueConditionSpecification();
+		conditionSpecification.setCondition(condition);
+		actionLink.setCondition(conditionSpecification);
 	}
 	
 }

efab285a629f93acd6a3f58d247e11876a65d5b7
au@andreasunger.net <au@andreasunger.net@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-04-25 08:56:07 +0000

    WhileLoopCondition's border item locator's preferred side changed.

--- DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/internal/editparts/WhileLoopConditionEditPart.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/internal/editparts/WhileLoopConditionEditPart.java
@@ -11,16 +11,11 @@
 
 package org.eclipselabs.damos.diagram.ui.internal.editparts;
 
-import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.AbstractBorderItemEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.figures.BorderItemLocator;
-import org.eclipse.gmf.runtime.diagram.ui.figures.IBorderItemLocator;
 import org.eclipse.gmf.runtime.gef.ui.figures.NodeFigure;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipselabs.damos.diagram.ui.editpolicies.IEditPolicyRoles;
-import org.eclipselabs.damos.diagram.ui.figures.IFigureConstants;
 import org.eclipselabs.damos.diagram.ui.internal.editpolicies.TerminalEditPolicy;
-import org.eclipselabs.damos.diagram.ui.internal.figures.CompoundConnectorFigure;
 import org.eclipselabs.damos.diagram.ui.internal.figures.IConnectorFigure;
 import org.eclipselabs.damos.diagram.ui.internal.figures.WhileLoopConditionFigure;
 import org.eclipselabs.damos.dml.Connector;
@@ -31,13 +26,6 @@ import org.eclipselabs.damos.dml.Connector;
  */
 public class WhileLoopConditionEditPart extends AbstractBorderItemEditPart implements IConnectorEditPart {
 
-	private static final Dimension BORDER_ITEM_OFFSET;
-	
-	static {
-		BORDER_ITEM_OFFSET = CompoundConnectorFigure.DEFAULT_SIZE.getScaled(0.5);
-		BORDER_ITEM_OFFSET.expand(IFigureConstants.DEFAULT_LINE_WIDTH, IFigureConstants.DEFAULT_LINE_WIDTH);
-	}
-	
 	/**
 	 * @param view
 	 */
@@ -62,19 +50,6 @@ public class WhileLoopConditionEditPart extends AbstractBorderItemEditPart imple
 		return new WhileLoopConditionFigure();
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.gmf.runtime.diagram.ui.editparts.AbstractBorderItemEditPart#getBorderItemLocator()
-	 */
-	@Override
-	public IBorderItemLocator getBorderItemLocator() {
-		IBorderItemLocator locator = super.getBorderItemLocator();
-		if (locator instanceof BorderItemLocator) {
-			BorderItemLocator borderItemLocator = (BorderItemLocator) locator;
-			borderItemLocator.setBorderItemOffset(BORDER_ITEM_OFFSET);
-		}
-		return locator;
-	}
-	
 	/* (non-Javadoc)
 	 * @see org.eclipse.gmf.runtime.diagram.ui.editparts.AbstractBorderItemEditPart#refreshBounds()
 	 */

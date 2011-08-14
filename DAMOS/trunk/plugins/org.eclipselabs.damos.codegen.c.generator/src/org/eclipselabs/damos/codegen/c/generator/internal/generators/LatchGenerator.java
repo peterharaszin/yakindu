101a9d1eefc79cb65919346489b0cb96654c2378
au@andreasunger.net <au@andreasunger.net@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-08-14 11:37:52 +0000

    Initial LatchGenerator added.

---
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.codegen.c.generator/src/org/eclipselabs/damos/codegen/c/generator/internal/generators/LatchGenerator.java
@@ -0,0 +1,48 @@
+/****************************************************************************
+ * Copyright (c) 2008, 2011 Andreas Unger and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andreas Unger - initial API and implementation 
+ ****************************************************************************/
+
+package org.eclipselabs.damos.codegen.c.generator.internal.generators;
+
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipselabs.damos.codegen.c.generator.AbstractComponentGenerator;
+import org.eclipselabs.damos.codegen.c.generator.IVariableAccessor;
+import org.eclipselabs.damos.codegen.c.generator.internal.util.InternalGeneratorUtil;
+
+/**
+ * @author Andreas Unger
+ *
+ */
+public class LatchGenerator extends AbstractComponentGenerator {
+
+	/* (non-Javadoc)
+	 * @see org.eclipselabs.damos.codegen.c.generator.AbstractComponentGenerator#contributesComputeOutputsCode()
+	 */
+	@Override
+	public boolean contributesComputeOutputsCode() {
+		return true;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipselabs.damos.codegen.c.generator.AbstractComponentGenerator#generateComputeOutputsCode(java.io.Writer, org.eclipselabs.damos.codegen.c.generator.IVariableAccessor, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	public void generateComputeOutputsCode(Writer writer, IVariableAccessor variableAccessor, IProgressMonitor monitor) throws CoreException {
+		PrintWriter printWriter = new PrintWriter(writer);
+		printWriter.printf("/* TODO: lock */\n");
+		printWriter.printf("%s = %s_data;\n", variableAccessor.getOutputVariable(getComponent().getFirstOutputPort(), false), InternalGeneratorUtil.getPrefix(getGenModel(), getNode()) + getComponent().getName());
+		printWriter.printf("/* TODO: unlock */\n");
+	}
+	
+}

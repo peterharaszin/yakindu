6739b230ca29b70d064c3177ec6e8288fc1041c5
au@andreasunger.com <au@andreasunger.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-10-22 09:16:37 +0000

    GeneratorFragment removed from DMLText.

--- DAMOS/trunk/plugins/org.eclipselabs.damos.dmltext/src-gen/org/eclipselabs/damos/dmltext/AbstractDMLTextRuntimeModule.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.dmltext/src-gen/org/eclipselabs/damos/dmltext/AbstractDMLTextRuntimeModule.java
@@ -129,11 +129,6 @@ public abstract class AbstractDMLTextRuntimeModule extends DefaultRuntimeModule 
 		binder.bind(org.eclipse.xtext.resource.IResourceDescriptions.class).annotatedWith(com.google.inject.name.Names.named(org.eclipse.xtext.resource.impl.ResourceDescriptionsProvider.NAMED_BUILDER_SCOPE)).to(org.eclipse.xtext.resource.impl.ResourceSetBasedResourceDescriptions.class);
 	}
 
-	// contributed by org.eclipse.xtext.generator.generator.GeneratorFragment
-	public Class<? extends org.eclipse.xtext.generator.IGenerator> bindIGenerator() {
-		return org.eclipselabs.damos.dmltext.generator.DMLTextGenerator.class;
-	}
-
 	// contributed by org.eclipse.xtext.generator.formatting.FormatterFragment
 	public Class<? extends org.eclipse.xtext.formatting.IFormatter> bindIFormatter() {
 		return org.eclipselabs.damos.dmltext.formatting.DMLTextFormatter.class;

b2133332e209f51c3393ab52727ec9e5bf55a88c
au@andreasunger.com <au@andreasunger.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2011-12-20 20:04:28 +0000

    EquationCompoundHelper refactorings.

--- DAMOS/trunk/plugins/org.eclipselabs.damos.mscript/src/org/eclipselabs/damos/mscript/functionmodel/util/EquationCompoundHelper.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.mscript/src/org/eclipselabs/damos/mscript/functionmodel/util/EquationCompoundHelper.java
@@ -12,18 +12,14 @@
 package org.eclipselabs.damos.mscript.functionmodel.util;
 
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.Set;
 
-import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipselabs.damos.mscript.functionmodel.EquationDescriptor;
 import org.eclipselabs.damos.mscript.functionmodel.FunctionDescriptor;
-import org.eclipselabs.damos.mscript.internal.functionmodel.util.InternalFunctionModelUtil.EquationIterator;
+import org.eclipselabs.damos.mscript.internal.functionmodel.util.InternalFunctionModelUtil;
 
 /**
  * @author Andreas Unger
@@ -31,40 +27,41 @@ import org.eclipselabs.damos.mscript.internal.functionmodel.util.InternalFunctio
  */
 public class EquationCompoundHelper {
 
-	private static final EquationComparator EQUATION_COMPARATOR = new EquationComparator();
-
 	public Collection<List<EquationDescriptor>> getEquationCompounds(FunctionDescriptor functionDescriptor) {
 		List<EquationDescriptor> backlog = new LinkedList<EquationDescriptor>(functionDescriptor.getEquationDescriptors());
 		Collection<List<EquationDescriptor>> equationCompounds = new LinkedList<List<EquationDescriptor>>();
 		while (!backlog.isEmpty()) {
-			List<EquationDescriptor> equationDescriptors = getNextEquationBlock(backlog);
+			List<EquationDescriptor> equationDescriptors = getNextEquationCompound(backlog);
 			equationCompounds.add(equationDescriptors);
 		}
 		return equationCompounds;
 	}
 	
-	private List<EquationDescriptor> getNextEquationBlock(List<EquationDescriptor> backlog) {
+	private List<EquationDescriptor> getNextEquationCompound(List<EquationDescriptor> backlog) {
 		List<EquationDescriptor> equationCompound = new LinkedList<EquationDescriptor>();
+		equationCompound.add(backlog.remove(0));
 		
 		boolean changed;
 		do {
 			changed = false;
 			for (Iterator<EquationDescriptor> backlogIt = backlog.iterator(); backlogIt.hasNext();) {
 				EquationDescriptor backlogEquationDescriptor = backlogIt.next();
-				int compareResult = 1;
+				EquationDescriptor next = null;
+				
 				for (ListIterator<EquationDescriptor> it = equationCompound.listIterator(); it.hasNext();) {
-					EquationDescriptor next = it.next();
-					compareResult = EQUATION_COMPARATOR.compare(backlogEquationDescriptor, next);
-					if (compareResult < 0) {
+					next = it.next();
+					if (InternalFunctionModelUtil.isDefinedBy(next, backlogEquationDescriptor)) {
 						it.previous();
 						it.add(backlogEquationDescriptor);
 						it.next();
 						backlogIt.remove();
 						changed = true;
+						next = null;
 						break;
 					}
 				}
-				if (compareResult > 0) {
+				
+				if (next != null && InternalFunctionModelUtil.isDefinedBy(backlogEquationDescriptor, next)) {
 					equationCompound.add(backlogEquationDescriptor);
 					backlogIt.remove();
 					changed = true;
@@ -75,38 +72,4 @@ public class EquationCompoundHelper {
 		return equationCompound;
 	}
 	
-	private static class EquationComparator implements Comparator<EquationDescriptor> {
-		
-		public int compare(EquationDescriptor equationDescriptor1, EquationDescriptor equationDescriptor2) {
-			Set<EquationDescriptor> equations = new HashSet<EquationDescriptor>();
-			TreeIterator<EquationDescriptor> definingEquations = new EquationIterator(equationDescriptor1);
-			while (definingEquations.hasNext()) {
-				EquationDescriptor next = definingEquations.next();
-				// Break cycle
-				if (!equations.add(next)) {
-					return 0;
-				}
-				if (next == equationDescriptor2) {
-					return 1;
-				}
-			}
-
-			equations = new HashSet<EquationDescriptor>();
-			definingEquations = new EquationIterator(equationDescriptor2);
-			while (definingEquations.hasNext()) {
-				EquationDescriptor next = definingEquations.next();
-				// Break cycle
-				if (!equations.add(next)) {
-					return 0;
-				}
-				if (next == equationDescriptor1) {
-					return -1;
-				}
-			}
-			
-			return 0;
-		}
-
-	}
-
 }

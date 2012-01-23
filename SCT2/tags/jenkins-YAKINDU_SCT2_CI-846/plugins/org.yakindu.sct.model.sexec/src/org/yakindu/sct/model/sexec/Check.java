67049c2decebacfb69d50bf3d68f041335028fc1
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-01-23 09:11:19 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-846.

---
+++ SCT2/tags/jenkins-YAKINDU_SCT2_CI-846/plugins/org.yakindu.sct.model.sexec/src/org/yakindu/sct/model/sexec/Check.java
@@ -0,0 +1,74 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.yakindu.sct.model.sexec;
+
+import org.eclipse.emf.common.util.EList;
+import org.yakindu.sct.model.sgraph.Statement;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Check</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.yakindu.sct.model.sexec.Check#getCondition <em>Condition</em>}</li>
+ *   <li>{@link org.yakindu.sct.model.sexec.Check#getRefs <em>Refs</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.yakindu.sct.model.sexec.SexecPackage#getCheck()
+ * @model
+ * @generated
+ */
+public interface Check extends Step {
+	/**
+	 * Returns the value of the '<em><b>Condition</b></em>' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Condition</em>' containment reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Condition</em>' containment reference.
+	 * @see #setCondition(Statement)
+	 * @see org.yakindu.sct.model.sexec.SexecPackage#getCheck_Condition()
+	 * @model containment="true"
+	 * @generated
+	 */
+	Statement getCondition();
+
+	/**
+	 * Sets the value of the '{@link org.yakindu.sct.model.sexec.Check#getCondition <em>Condition</em>}' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Condition</em>' containment reference.
+	 * @see #getCondition()
+	 * @generated
+	 */
+	void setCondition(Statement value);
+
+	/**
+	 * Returns the value of the '<em><b>Refs</b></em>' reference list.
+	 * The list contents are of type {@link org.yakindu.sct.model.sexec.CheckRef}.
+	 * It is bidirectional and its opposite is '{@link org.yakindu.sct.model.sexec.CheckRef#getCheck <em>Check</em>}'.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Refs</em>' reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Refs</em>' reference list.
+	 * @see org.yakindu.sct.model.sexec.SexecPackage#getCheck_Refs()
+	 * @see org.yakindu.sct.model.sexec.CheckRef#getCheck
+	 * @model opposite="check"
+	 * @generated
+	 */
+	EList<CheckRef> getRefs();
+
+} // Check

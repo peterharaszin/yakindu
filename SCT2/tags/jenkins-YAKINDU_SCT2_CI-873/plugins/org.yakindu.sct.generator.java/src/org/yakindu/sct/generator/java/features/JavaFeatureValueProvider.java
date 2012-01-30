184b28b374aa485f146bc9222e98f3f4aa2c3513
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-01-30 11:10:24 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-873.

---
+++ SCT2/tags/jenkins-YAKINDU_SCT2_CI-873/plugins/org.yakindu.sct.generator.java/src/org/yakindu/sct/generator/java/features/JavaFeatureValueProvider.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2011 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
+package org.yakindu.sct.generator.java.features;
+
+import static org.yakindu.sct.generator.java.features.IJavaFeatureConstants.BASE_PACKAGE;
+import static org.yakindu.sct.generator.java.features.IJavaFeatureConstants.IMPLEMENTATION_SUFFIX;
+import static org.yakindu.sct.generator.java.features.IJavaFeatureConstants.LIBRARY_NAME;
+import static org.yakindu.sct.generator.java.features.IJavaFeatureConstants.JAVA_KEYWORDS;
+import org.yakindu.sct.generator.java.features.JavaFeatureConstants;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.yakindu.sct.generator.core.features.AbstractDefaultFeatureValueProvider;
+import org.yakindu.sct.model.sgen.FeatureParameterValue;
+import org.yakindu.sct.model.sgen.FeatureTypeLibrary;
+import org.yakindu.sct.model.sgraph.Statechart;
+
+/**
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+public class JavaFeatureValueProvider extends
+		AbstractDefaultFeatureValueProvider {
+
+	private static final String PACKAGE_NAME_REGEX = "([a-zA-Z_][a-zA-Z0-9_]*\\.)+[a-zA-Z_][a-zA-Z0-9_]*";
+	private static final String SUFFIX_REGEX = "[a-zA-Z0-9_]*";
+
+	@Override
+	protected void setDefaultValue(FeatureParameterValue parameterValue,
+			Statechart statechart) {
+		if (parameterValue.getParameter().getName().equals(BASE_PACKAGE)) {
+			parameterValue.setValue("org.yakindu.sct."
+					+ JavaFeatureConstants.getValidStatemachineName(statechart.getName()));
+		} else if (parameterValue.getParameter().getName()
+				.equals(IMPLEMENTATION_SUFFIX)) {
+			parameterValue.setValue("impl");
+		}
+	}
+
+	public boolean isProviderFor(FeatureTypeLibrary library) {
+		return library.getName().equals(LIBRARY_NAME);
+	}
+
+	public IStatus validateParameterValue(FeatureParameterValue value) {
+		String name = value.getParameter().getName();
+		if (BASE_PACKAGE.equals(name)) {
+			if (!value.getStringValue().matches(PACKAGE_NAME_REGEX)) {
+				return error("Invalid package name");
+			}
+			// Filter out java keywords
+			for (String keyword : Arrays.asList(JAVA_KEYWORDS)) {
+				Pattern pattern = Pattern.compile("(?:^|\\.)" + keyword
+						+ "(?:$|\\.)");
+				Matcher matcher = pattern.matcher(value.getStringValue());
+				while (matcher.find()) {
+					return error("Java keyword '" + matcher.group()
+							+ "' is not allowed in package names.");
+				}
+			}
+		}
+		if (IMPLEMENTATION_SUFFIX.equals(name)) {
+			if (!value.getStringValue().matches(SUFFIX_REGEX)) {
+				return error("Invalid value");
+			}
+			for (String keyword : Arrays.asList(JAVA_KEYWORDS)) {
+				Pattern pattern = Pattern.compile("^" + keyword + "$");
+				Matcher matcher = pattern.matcher(value.getStringValue());
+				while (matcher.find()) {
+					return error("Java keyword '" + matcher.group()
+							+ "' is not allowed as suffix.");
+				}
+			}
+
+		}
+		return Status.OK_STATUS;
+	}
+}

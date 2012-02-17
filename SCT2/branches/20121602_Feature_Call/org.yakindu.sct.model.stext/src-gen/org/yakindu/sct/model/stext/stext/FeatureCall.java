5a72146c0d430d2290655652574abb56321c5e95
A.Muelder@googlemail.com <A.Muelder@googlemail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-02-17 10:00:03 +0000

    Share project "org.yakindu.sct.model.stext" into "https://svn.codespot.com/a/eclipselabs.org/yakindu"

---
+++ SCT2/branches/20121602_Feature_Call/org.yakindu.sct.model.stext/src-gen/org/yakindu/sct/model/stext/stext/FeatureCall.java
@@ -0,0 +1,82 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package org.yakindu.sct.model.stext.stext;
+
+import org.yakindu.base.types.Feature;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Feature Call</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.yakindu.sct.model.stext.stext.FeatureCall#getOwner <em>Owner</em>}</li>
+ *   <li>{@link org.yakindu.sct.model.stext.stext.FeatureCall#getFeature <em>Feature</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.yakindu.sct.model.stext.stext.StextPackage#getFeatureCall()
+ * @model
+ * @generated
+ */
+public interface FeatureCall extends Expression
+{
+  /**
+   * Returns the value of the '<em><b>Owner</b></em>' containment reference.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Owner</em>' containment reference isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Owner</em>' containment reference.
+   * @see #setOwner(Expression)
+   * @see org.yakindu.sct.model.stext.stext.StextPackage#getFeatureCall_Owner()
+   * @model containment="true"
+   * @generated
+   */
+  Expression getOwner();
+
+  /**
+   * Sets the value of the '{@link org.yakindu.sct.model.stext.stext.FeatureCall#getOwner <em>Owner</em>}' containment reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Owner</em>' containment reference.
+   * @see #getOwner()
+   * @generated
+   */
+  void setOwner(Expression value);
+
+  /**
+   * Returns the value of the '<em><b>Feature</b></em>' reference.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Feature</em>' reference isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Feature</em>' reference.
+   * @see #setFeature(Feature)
+   * @see org.yakindu.sct.model.stext.stext.StextPackage#getFeatureCall_Feature()
+   * @model
+   * @generated
+   */
+  Feature getFeature();
+
+  /**
+   * Sets the value of the '{@link org.yakindu.sct.model.stext.stext.FeatureCall#getFeature <em>Feature</em>}' reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Feature</em>' reference.
+   * @see #getFeature()
+   * @generated
+   */
+  void setFeature(Feature value);
+
+} // FeatureCall

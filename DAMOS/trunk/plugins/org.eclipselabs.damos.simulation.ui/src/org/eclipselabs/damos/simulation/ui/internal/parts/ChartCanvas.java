45300e1cd0347a92f09a64d12ae8e8e74379f643
au@andreasunger.com <au@andreasunger.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-03-03 17:56:16 +0000

    Simulation result charts switched to SWTChart.

--- DAMOS/trunk/plugins/org.eclipselabs.damos.simulation.ui/src/org/eclipselabs/damos/simulation/ui/internal/parts/ChartCanvas.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.simulation.ui/src/org/eclipselabs/damos/simulation/ui/internal/parts/ChartCanvas.java
@@ -11,78 +11,45 @@
 
 package org.eclipselabs.damos.simulation.ui.internal.parts;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
-import org.eclipse.birt.chart.device.IDeviceRenderer;
-import org.eclipse.birt.chart.exception.ChartException;
-import org.eclipse.birt.chart.factory.Generator;
-import org.eclipse.birt.chart.model.attribute.Bounds;
-import org.eclipse.birt.chart.model.attribute.impl.BoundsImpl;
-import org.eclipse.birt.chart.util.PluginSettings;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ControlAdapter;
-import org.eclipse.swt.events.ControlEvent;
-import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.events.PaintListener;
-import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.custom.StackLayout;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipselabs.damos.simulation.IXYChartData;
-import org.eclipselabs.damos.simulation.ui.SimulationUIPlugin;
+import org.swtchart.Chart;
+import org.swtchart.ILineSeries;
+import org.swtchart.ILineSeries.PlotSymbolType;
+import org.swtchart.ISeries.SeriesType;
+import org.swtchart.ext.InteractiveChart;
 
 /**
  * @author Andreas Unger
  *
  */
-public class ChartCanvas extends Canvas implements PaintListener {
+public class ChartCanvas extends Composite {
 
-	private IDeviceRenderer renderer;
-	private Image cachedImage;
-	private List<ChartContext> chartContexts = new ArrayList<ChartContext>();
 	private int singleChartIndex = -1;
 
 	private boolean realtime;
 	private long progress = -1;
 	
+	private boolean chartsAvailable; 
+	private Label messageLabel;
+
 	public ChartCanvas(Composite parent, int style) {
 		super(parent, style);
-		
 		setBackground(getDisplay().getSystemColor(SWT.COLOR_WHITE));
-		
-		try {
-			PluginSettings ps = PluginSettings.instance();
-			renderer = ps.getDevice("dv.SWT");
-		} catch (ChartException e) {
-			logChartError(e);
-		}
-		
-		addControlListener(new ControlAdapter() {
-			
-			public void controlResized(ControlEvent e) {
-				if (cachedImage != null) {
-					cachedImage.dispose();
-					cachedImage = null;
-				}
-			}
-			
-		});
-		addPaintListener(this);
-	}
-	
-	public void clear() {
-		chartContexts.clear();
-		redraw();
+		updateCanvas();
 	}
 	
 	/**
-	 * @param realtime the realtime to set
+	 * @param realtime the real-time to set
 	 */
 	public void setRealtime(boolean realtime) {
 		this.realtime = realtime;
@@ -94,141 +61,120 @@ public class ChartCanvas extends Canvas implements PaintListener {
 	public void setProgress(long progress) {
 		if (this.progress != progress) {
 			this.progress = progress;
-			redraw();
+			updateCanvas();
 		}
 	}
 		
-	public void setDataset(Collection<IXYChartData> dataset) {
-		chartContexts.clear();
-		for (IXYChartData data : dataset) {
-			ChartContext context = new ChartContext();
-			context.initializeChart();
-			context.setChartData(data);
-			chartContexts.add(context);
-		}
-		if (cachedImage != null) {
-			cachedImage.dispose();
-			cachedImage = null;
-		}
-		redraw();
-	}
-	
 	public void setSingleChartIndex(int index) {
 		if (singleChartIndex != index) {
 			singleChartIndex = index;
-			if (chartContexts.size() > 1) {
-				if (cachedImage != null) {
-					cachedImage.dispose();
-					cachedImage = null;
-				}
-				redraw();
+			updateCanvas();
+		}
+	}
+
+	public void setDataset(Collection<IXYChartData> dataset) {
+		disposeChildren();
+		chartsAvailable = !dataset.isEmpty();
+		updateCanvas();
+		
+		for (IXYChartData data : dataset) {
+			Chart chart = new InteractiveChart(this, SWT.NONE);
+			chart.getTitle().setText(data.getChartTitle());
+			chart.getLegend().setVisible(false);
+			chart.getAxisSet().getXAxis(0).getTitle().setVisible(false);
+			chart.getAxisSet().getYAxis(0).getTitle().setVisible(false);
+			chart.setBackground(getDisplay().getSystemColor(SWT.COLOR_WHITE));
+			
+			double[] xValues = data.getXValues();
+			double[][] yValues = data.getYValues();
+			
+			for (int i = 0; i < yValues.length; ++i) {
+				ILineSeries lineSeries = (ILineSeries) chart.getSeriesSet().createSeries(SeriesType.LINE, "Series " + (i + 1));
+				lineSeries.setLineColor(getLineColor(i));
+				lineSeries.setSymbolType(PlotSymbolType.NONE);
+				lineSeries.setXSeries(xValues);
+				lineSeries.setYSeries(yValues[i]);
+			}
+		}
+
+		// Adjust range *after* we added all charts
+		for (Control control : getChildren()) {
+			if (control instanceof Chart) {
+				((Chart) control).getAxisSet().adjustRange();
 			}
 		}
 	}
 	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.swt.events.PaintListener#paintControl(org.eclipse.swt.events.PaintEvent)
-	 */
-	public void paintControl(PaintEvent e) {
-		Composite composite = (Composite) e.getSource();
-		Rectangle size = composite.getClientArea();
-
+	private Color getLineColor(int index) {
+		switch (index) {
+		case 0:
+			return getDisplay().getSystemColor(SWT.COLOR_BLUE);
+		case 1:
+			return getDisplay().getSystemColor(SWT.COLOR_RED);
+		case 2:
+			return getDisplay().getSystemColor(SWT.COLOR_GREEN);
+		case 3:
+			return getDisplay().getSystemColor(SWT.COLOR_MAGENTA);
+		case 4:
+			return getDisplay().getSystemColor(SWT.COLOR_DARK_YELLOW);
+		case 5:
+			return getDisplay().getSystemColor(SWT.COLOR_DARK_CYAN);
+		case 6:
+			return getDisplay().getSystemColor(SWT.COLOR_DARK_BLUE);
+		case 7:
+			return getDisplay().getSystemColor(SWT.COLOR_DARK_RED);
+		case 8:
+			return getDisplay().getSystemColor(SWT.COLOR_DARK_GREEN);
+		}
+		return getDisplay().getSystemColor(SWT.COLOR_BLACK);
+	}
+	
+	private void updateCanvas() {
 		String message = null;
-		
 		if (progress >= 0) {
 			message = String.format("Simulating... %d%s", progress, realtime ? "s" : "%");
-		} else if (!chartContexts.isEmpty()) {
-			if (cachedImage == null) {
-				buildChart(e.gc);
-				drawToCachedImage(size);
+		} else if (chartsAvailable) {
+			if (singleChartIndex < 0 || singleChartIndex >= getChildren().length) {
+				setLayout(new TileLayout(2));
+			} else {
+				StackLayout layout = new StackLayout();
+				layout.topControl = getChildren()[singleChartIndex];
+				setLayout(layout);
 			}
-			e.gc.drawImage(
-					cachedImage,
-					0, 0, cachedImage.getBounds().width, cachedImage.getBounds().height,
-					0, 0, size.width, size.height);
 		} else {
-			message = "No simulation data available.";
+			message = "No chart data available.";
 		}
 		
 		if (message != null) {
-			Point extent = e.gc.stringExtent(message);
-			e.gc.drawText(message, (size.width - extent.x) / 2, (size.height - extent.y) / 2);
+			setMessage(message);
 		}
+
+		layout();
 	}
 
-	private void buildChart(GC gc) {
-		Point size = getSize();
-		double scale = 72.0 / renderer.getDisplayServer().getDpiResolution();
-		renderer.setProperty(IDeviceRenderer.GRAPHICS_CONTEXT, gc);
-		if (singleChartIndex < 0) {
-			size.y /= chartContexts.size();
-			for (int i = 0; i < chartContexts.size(); ++i) {
-				Bounds bounds = BoundsImpl.create(0, i * size.y, size.x, size.y);
-				bounds.scale(scale);
-				buildChart(i, bounds);
-			}
-		} else {
-			Bounds bounds = BoundsImpl.create(0, 0, size.x, size.y);
-			bounds.scale(scale);
-			buildChart(singleChartIndex, bounds);
-		}
-	}
-	
-	private void buildChart(int index, Bounds bounds) {
-		try {
-			chartContexts.get(index).setState(Generator.instance().build(
-					renderer.getDisplayServer(),
-					chartContexts.get(index).getChart(),
-					bounds, null, null, null));
-		} catch (ChartException e) {
-			logChartError(e);
-		}
-	}
-
-	private void drawToCachedImage(Rectangle size) {
-		GC gc = null;
-		try {
-			if (cachedImage != null) {
-				cachedImage.dispose();
-			}
-			cachedImage = new Image(getDisplay(), size);
-
-			gc = new GC(cachedImage);
-			renderer.setProperty(IDeviceRenderer.GRAPHICS_CONTEXT, gc);
-
-			if (singleChartIndex < 0) {
-				for (int i = 0; i < chartContexts.size(); ++i) {
-					Generator.instance().render(renderer, chartContexts.get(i).getState());
-				}
-			} else {
-				Generator.instance().render(renderer, chartContexts.get(singleChartIndex).getState());
-			}
-		} catch (ChartException e) {
-			logChartError(e);
-		} finally {
-			if (gc != null) {
-				gc.dispose();
-			}
-		}
-	}
-		
-	private void logChartError(Exception e) {
-		SimulationUIPlugin.getDefault().getLog().log(
-				new Status(IStatus.ERROR, SimulationUIPlugin.PLUGIN_ID,
-						"Chart error", e));
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.swt.widgets.Widget#dispose()
+	/**
+	 * @param message
 	 */
-	public void dispose() {
-		if (cachedImage != null) {
-			cachedImage.dispose();
+	private void setMessage(String message) {
+		if (messageLabel == null) {
+			disposeChildren();
+			setLayout(new GridLayout());
+			messageLabel = new Label(this, SWT.NONE);
+			messageLabel.setBackground(getDisplay().getSystemColor(SWT.COLOR_WHITE));
+			messageLabel.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true));
 		}
-		super.dispose();
+		messageLabel.setText(message);
 	}
-	
+
+	/**
+	 * 
+	 */
+	private void disposeChildren() {
+		for (Control control : getChildren()) {
+			control.dispose();
+		}
+		messageLabel = null;
+		chartsAvailable = false;
+	}
+
 }

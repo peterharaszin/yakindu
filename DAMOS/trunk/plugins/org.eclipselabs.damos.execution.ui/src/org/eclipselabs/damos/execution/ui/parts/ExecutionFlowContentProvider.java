4734f662a682cc0527970091ccfe88856e2bec5e
au@andreasunger.com <au@andreasunger.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-03-22 17:18:08 +0000

    Execution UI plug-in added.

---
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.execution.ui/src/org/eclipselabs/damos/execution/ui/parts/ExecutionFlowContentProvider.java
@@ -0,0 +1,79 @@
+/****************************************************************************
+ * Copyright (c) 2008, 2012 Andreas Unger and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andreas Unger - initial API and implementation 
+ ****************************************************************************/
+
+package org.eclipselabs.damos.execution.ui.parts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.zest.core.viewers.IGraphEntityContentProvider;
+import org.eclipselabs.damos.execution.Edge;
+import org.eclipselabs.damos.execution.ExecutionFlow;
+import org.eclipselabs.damos.execution.Graph;
+import org.eclipselabs.damos.execution.Node;
+
+/**
+ * @author Andreas Unger
+ *
+ */
+public class ExecutionFlowContentProvider implements IGraphEntityContentProvider {
+
+	private static final Object[] EMPTY = new Object[0];
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+	 */
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.zest.core.viewers.IGraphEntityContentProvider#getElements(java.lang.Object)
+	 */
+	public Object[] getElements(Object inputElement) {
+		if (inputElement instanceof ExecutionFlow) {
+			ExecutionFlow executionFlow = (ExecutionFlow) inputElement;
+			List<Object> elements = new ArrayList<Object>();
+			elements.addAll(executionFlow.getTaskGraphs());
+			elements.addAll(executionFlow.getGraph().getNodes());
+			return elements.toArray();
+		}
+		if (inputElement instanceof Graph) {
+			return ((Graph) inputElement).getNodes().toArray();
+		}
+		return EMPTY;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.zest.core.viewers.IGraphEntityContentProvider#getConnectedTo(java.lang.Object)
+	 */
+	public Object[] getConnectedTo(Object entity) {
+		if (entity instanceof Node) {
+			Node node = (Node) entity;
+			EList<Edge> outgoingEdges = node.getOutgoingEdges();
+			Object[] outgoingNodes = new Object[outgoingEdges.size()];
+			for (int i = 0; i < outgoingNodes.length; ++i) {
+				outgoingNodes[i] = outgoingEdges.get(i).getTarget();
+			}
+			return outgoingNodes;
+		}
+		return EMPTY;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+	 */
+	public void dispose() {
+	}
+
+}

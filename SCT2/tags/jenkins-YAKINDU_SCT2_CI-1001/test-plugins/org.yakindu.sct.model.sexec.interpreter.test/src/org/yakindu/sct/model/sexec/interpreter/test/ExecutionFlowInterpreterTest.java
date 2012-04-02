14e6e6cd39abffcf83aa86672094307004c681e9
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-04-02 12:15:39 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-1001.

---
+++ SCT2/tags/jenkins-YAKINDU_SCT2_CI-1001/test-plugins/org.yakindu.sct.model.sexec.interpreter.test/src/org/yakindu/sct/model/sexec/interpreter/test/ExecutionFlowInterpreterTest.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2012 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
+package org.yakindu.sct.model.sexec.interpreter.test;
+
+import java.io.IOException;
+
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import util.AbstractExecutionFlowTest;
+import util.SExecInjectionProvider;
+
+/**
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+@RunWith(XtextRunner.class)
+@InjectWith(SExecInjectionProvider.class)
+public class ExecutionFlowInterpreterTest extends AbstractExecutionFlowTest {
+
+	@Test
+	public void testSimpleGuard() throws Exception {
+		loadAndconfigureInterpreter(models.createGuardModel());
+		assertIsActive("A");
+		assertVarValue("MyVar", 0);
+		context().raiseEvent("Event1", null);
+		interpreter.runCycle();
+		assertIsActive("A");
+		context().raiseEvent("Event2", null);
+		interpreter.runCycle();
+		assertVarValue("MyVar", 10);
+		assertIsActive("B");
+		context().raiseEvent("Return", null);
+		interpreter.runCycle();
+		assertIsActive("A");
+		context().raiseEvent("Event1", null);
+		interpreter.runCycle();
+		assertIsActive("B");
+	}
+
+	@Test
+	public void testsimpleHierachy() throws IOException {
+		loadAndconfigureInterpreter(models.createSimpleHierachyModel());
+		assertIsActive("A");
+		context().raiseEvent("Event1", null);
+		interpreter.runCycle();
+		assertIsActive("B");
+		assertIsActive("B1");
+	}
+
+	@Test
+	public void testDeepHistory() throws IOException {
+		loadAndconfigureInterpreter(models.createDeepHistoryModel());
+		assertIsActive("State1");
+		context().raiseEvent("event1", null);
+		interpreter.runCycle();
+		interpreter.runCycle();
+		interpreter.runCycle();
+		assertIsActive("State2");
+		assertIsActive("State4");
+		assertIsActive("State6");
+		context().raiseEvent("event2", null);
+		interpreter.runCycle();
+		assertIsActive("State1");
+		context().raiseEvent("event1", null);
+		interpreter.runCycle();
+		interpreter.runCycle();
+		interpreter.runCycle();
+		// Now, the deep history states should be active
+		assertIsActive("State2");
+		assertIsActive("State4");
+		assertIsActive("State6");
+	}
+
+	@Test
+	public void testStateIsActive() throws IOException {
+		loadAndconfigureInterpreter(models.createStateIsActiveModel());
+		assertIsActive("R1A");
+		assertIsActive("R2A");
+		context().raiseEvent("Event1", null);
+		interpreter.runCycle();
+		interpreter.runCycle();
+		assertIsActive("R1B");
+		assertIsActive("R2B");
+	}
+
+	@Test
+	public void testValuedEvents() throws IOException {
+		loadAndconfigureInterpreter(models.createValuedEventsModel());
+		assertIsActive("A");
+		assertIsActive("B");
+		interpreter.runCycle();
+		assertIsActive("C");
+		assertVarValue("myVar", 42);
+	}
+
+}

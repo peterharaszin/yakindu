a1f1131a2fdd4a25ecdb285fbc330ba4a263d9ac
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-05-03 14:11:01 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-1115.

---
+++ SCT2/tags/jenkins-YAKINDU_SCT2_CI-1115/examples/org.yakindu.sct.example.trafficlight/src-gen/org/yakindu/sct/runtime/java/NotificationType.java
@@ -0,0 +1,8 @@
+package org.yakindu.sct.runtime.java;
+
+/**
+ * Enum to define possible notification types.
+ */
+public enum NotificationType {
+	EventNotification, VariableNotification, RuntimeCycleNotification
+}

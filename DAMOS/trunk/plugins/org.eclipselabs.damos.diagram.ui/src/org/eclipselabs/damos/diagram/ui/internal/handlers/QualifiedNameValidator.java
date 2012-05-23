715b7188e3eaec93a8f3e2003e6408b86018d7c0
au@andreasunger.com <au@andreasunger.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-05-23 18:52:34 +0000

    New and rename fragment dialogs improved.

---
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/internal/handlers/QualifiedNameValidator.java
@@ -0,0 +1,28 @@
+/****************************************************************************
+ * Copyright (c) 2008, 2012 Andreas Unger and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andreas Unger - initial API and implementation 
+ ****************************************************************************/
+
+package org.eclipselabs.damos.diagram.ui.internal.handlers;
+
+import org.eclipse.jface.dialogs.IInputValidator;
+
+class QualifiedNameValidator implements IInputValidator {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.IInputValidator#isValid(java.lang.String)
+	 */
+	public String isValid(String newText) {
+		if (!RenameFragmentHandler.QUALIFIED_NAME_PATTERN.matcher(newText).matches() || newText.contains("__")) {
+			return "Invalid qualified name";
+		}
+		return null;
+	}
+	
+}

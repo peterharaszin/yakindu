ea7ebfbaa02cf21b93d709e345ab2f8ad45d9dba
au@andreasunger.com <au@andreasunger.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-05-23 18:52:34 +0000

    New and rename fragment dialogs improved.

--- DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/internal/handlers/NewFragmentHandler.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/internal/handlers/NewFragmentHandler.java
@@ -1,9 +1,13 @@
 package org.eclipselabs.damos.diagram.ui.internal.handlers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.emf.common.command.Command;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.edit.command.ChangeCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
@@ -26,7 +30,9 @@ public class NewFragmentHandler extends AbstractHandler {
 				Object element = structuredSelection.getFirstElement();
 				if (element instanceof Fragment) {
 					Fragment fragment = (Fragment) element;
-					InputDialog d = new InputDialog(HandlerUtil.getActiveShell(event), "Create Fragment", "Qualified name:", "org.example.Unnamed", null);
+					InputDialog d = new InputDialog(HandlerUtil.getActiveShell(event), "Create Fragment",
+							"Qualified name (e.g. mypackage.MyFragment):", findAvailableFragmentName(fragment),
+							new QualifiedNameValidator());
 					if (d.open() == Dialog.OK) {
 						Fragment newFragment = DMLFactory.eINSTANCE.createFragment();
 						newFragment.setQualifiedName(d.getValue());
@@ -40,12 +46,31 @@ public class NewFragmentHandler extends AbstractHandler {
 		}
 		return null;
 	}
-	
+
+	private String findAvailableFragmentName(Fragment fragment) {
+		String packageName = fragment.getName() != null ? fragment.getQualifiedName().toLowerCase() : "org.example";
+		String preferredName = packageName + ".Unnamed";
+		String name = preferredName;
+		if (fragment.eResource() != null) {
+			Set<String> usedNames = new HashSet<String>();
+			for (EObject eObject : fragment.eResource().getContents()) {
+				if (eObject instanceof Fragment) {
+					usedNames.add(((Fragment) eObject).getQualifiedName());
+				}
+			}
+			int i = 1;
+			while (usedNames.contains(name)) {
+				name = preferredName + ++i;
+			}
+		}
+		return name;
+	}
+
 	private static class AddFragmentCommand extends ChangeCommand {
 
 		private Resource resource;
 		private Fragment fragment;
-		
+
 		/**
 		 * @param notifier
 		 */
@@ -59,7 +84,7 @@ public class NewFragmentHandler extends AbstractHandler {
 		protected void doExecute() {
 			resource.getContents().add(fragment);
 		}
-		
+
 	}
 
 }
--- DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/internal/handlers/RenameFragmentHandler.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.diagram.ui/src/org/eclipselabs/damos/diagram/ui/internal/handlers/RenameFragmentHandler.java
@@ -5,23 +5,24 @@ import java.util.regex.Pattern;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.edit.command.CommandParameter;
 import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipselabs.damos.dml.DMLPackage;
 import org.eclipselabs.damos.dml.Fragment;
+import org.eclipselabs.damos.dml.util.FragmentSelectionManager;
 
 public class RenameFragmentHandler extends AbstractHandler {
 
-	private static final Pattern QUALIFIED_NAME_PATTERN = Pattern.compile("\\A([a-zA-Z]\\w*)(\\.[a-zA-Z]\\w*)+\\z");
+	static final Pattern QUALIFIED_NAME_PATTERN = Pattern.compile("\\A([a-zA-Z]\\w*)(\\.[a-zA-Z]\\w*)+\\z");
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
@@ -29,9 +30,17 @@ public class RenameFragmentHandler extends AbstractHandler {
 			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
 			if (!structuredSelection.isEmpty()) {
 				Object element = structuredSelection.getFirstElement();
+				if (element instanceof IAdaptable) {
+					FragmentSelectionManager fragmentSelectionManager = (FragmentSelectionManager) ((IAdaptable) element).getAdapter(FragmentSelectionManager.class);
+					if (fragmentSelectionManager != null && fragmentSelectionManager.getSelectedFragment() != null) {
+						element = fragmentSelectionManager.getSelectedFragment();
+					}
+				}
 				if (element instanceof Fragment) {
 					Fragment fragment = (Fragment) element;
-					InputDialog d = new InputDialog(HandlerUtil.getActiveShell(event), "Rename Fragment", "Qualified name (e.g. mypackage.MyFragment):", fragment.getQualifiedName(), new QualifiedNameValidator());
+					InputDialog d = new InputDialog(HandlerUtil.getActiveShell(event), "Rename Fragment",
+							"Qualified name (e.g. mypackage.MyFragment):", fragment.getQualifiedName(),
+							new QualifiedNameValidator());
 					if (d.open() == Dialog.OK) {
 						EditingDomain editingDomain = TransactionUtil.getEditingDomain(fragment);
 						Command command = editingDomain.createCommand(
@@ -43,20 +52,6 @@ public class RenameFragmentHandler extends AbstractHandler {
 			}
 		}
 		return null;
-	}
-	
-	private static class QualifiedNameValidator implements IInputValidator {
-
-		/* (non-Javadoc)
-		 * @see org.eclipse.jface.dialogs.IInputValidator#isValid(java.lang.String)
-		 */
-		public String isValid(String newText) {
-			if (!QUALIFIED_NAME_PATTERN.matcher(newText).matches() || newText.contains("__")) {
-				return "Invalid qualified name";
-			}
-			return null;
-		}
-		
 	}
 
 }

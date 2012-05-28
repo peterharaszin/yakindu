d5f90fb29b02a7d8c07b548e5d5df07d0f2c7cf9
au@andreasunger.com <au@andreasunger.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-05-28 12:34:34 +0000

    Tasks and problems view added to perspective.

--- DAMOS/trunk/plugins/org.eclipselabs.damos.ide.ui/src/org/eclipselabs/damos/ide/ui/internal/DamosPerspectiveFactory.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.ide.ui/src/org/eclipselabs/damos/ide/ui/internal/DamosPerspectiveFactory.java
@@ -1,5 +1,7 @@
 package org.eclipselabs.damos.ide.ui.internal;
 
+import org.eclipse.debug.ui.IDebugUIConstants;
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
@@ -7,18 +9,23 @@ public class DamosPerspectiveFactory implements IPerspectiveFactory {
 	
 	private static final String ID_FRAGMENT_EXPLORER_VIEW = "org.eclipselabs.damos.ide.ui.fragmentExplorerView";
 	private static final String ID_BLOCK_LIBRARY_VIEW = "org.eclipselabs.damos.ide.ui.blockLibraryView";
-	private static final String ID_LAUNCH_ACTION_SET = "org.eclipse.debug.ui.launchActionSet";
+	private static final String ID_CONSOLE_FOLDER_VIEW = "org.eclipselabs.damos.ide.ui.consoleFolder";
 
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 		
 		layout.addView(IPageLayout.ID_PROJECT_EXPLORER, IPageLayout.LEFT, 0.25f, editorArea);
 		layout.addView(ID_BLOCK_LIBRARY_VIEW, IPageLayout.BOTTOM, 0.5f, IPageLayout.ID_PROJECT_EXPLORER);
-		layout.addView(IPageLayout.ID_PROP_SHEET, IPageLayout.BOTTOM, 0.75f, editorArea);
+
+		IFolderLayout consoleFolder = layout.createFolder(ID_CONSOLE_FOLDER_VIEW, IPageLayout.BOTTOM, 0.75f, editorArea);
+		consoleFolder.addView(IPageLayout.ID_PROP_SHEET);
+		consoleFolder.addView(IPageLayout.ID_TASK_LIST);
+		consoleFolder.addView(IPageLayout.ID_PROBLEM_VIEW);
+
 		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.RIGHT, 0.75f, editorArea);
 		layout.addView(ID_FRAGMENT_EXPLORER_VIEW, IPageLayout.BOTTOM, 0.5f, IPageLayout.ID_OUTLINE);
 		
-		layout.addActionSet(ID_LAUNCH_ACTION_SET);
+		layout.addActionSet(IDebugUIConstants.LAUNCH_ACTION_SET);
 	}
 
 }

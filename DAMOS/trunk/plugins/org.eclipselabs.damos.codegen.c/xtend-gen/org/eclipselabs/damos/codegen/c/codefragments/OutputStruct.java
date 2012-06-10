25827734299b0bca8ab168180857f3dc9b26a55b
au@andreasunger.com <au@andreasunger.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-06-10 18:20:29 +0000

    CodeFragment configuration in Damos Codegen ported to Guice.

---
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.codegen.c/xtend-gen/org/eclipselabs/damos/codegen/c/codefragments/OutputStruct.java
@@ -0,0 +1,79 @@
+package org.eclipselabs.damos.codegen.c.codefragments;
+
+import com.google.inject.Inject;
+import java.util.List;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipselabs.damos.codegen.c.IComponentGenerator;
+import org.eclipselabs.damos.codegen.c.IComponentGeneratorContext;
+import org.eclipselabs.damos.codegen.c.IGeneratorContext;
+import org.eclipselabs.damos.codegen.c.codefragments.PrimaryCodeFragment;
+import org.eclipselabs.damos.codegen.c.internal.util.InternalGeneratorUtil;
+import org.eclipselabs.damos.codegen.c.util.GeneratorConfigurationUtil;
+import org.eclipselabs.damos.dconfig.Configuration;
+import org.eclipselabs.damos.dml.Component;
+import org.eclipselabs.damos.dml.InputPort;
+import org.eclipselabs.damos.execution.ComponentNode;
+import org.eclipselabs.damos.execution.datatype.IComponentSignature;
+import org.eclipselabs.damos.mscript.DataType;
+import org.eclipselabs.damos.mscript.codegen.c.util.MscriptGeneratorUtil;
+import org.eclipselabs.damos.mscript.computationmodel.ComputationModel;
+
+/**
+ * @author Andreas Unger
+ */
+@SuppressWarnings("all")
+public class OutputStruct extends PrimaryCodeFragment {
+  private CharSequence content;
+  
+  @Inject
+  public OutputStruct() {
+  }
+  
+  public void doInitialize(final IGeneratorContext context, final IProgressMonitor monitor) {
+    Configuration _configuration = context.getConfiguration();
+    final String prefix = GeneratorConfigurationUtil.getPrefix(_configuration);
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("typedef struct {");
+    _builder.newLine();
+    {
+      List<ComponentNode> _outportNodes = InternalGeneratorUtil.getOutportNodes(context);
+      for(final ComponentNode node : _outportNodes) {
+        _builder.append("\t");
+        String _cVariableDeclaration = this.getCVariableDeclaration(context, node);
+        _builder.append(_cVariableDeclaration, "	");
+        _builder.append(";");
+        _builder.newLineIfNotEmpty();
+      }
+    }
+    _builder.append("} ");
+    _builder.append(prefix, "");
+    _builder.append("Output;");
+    _builder.newLineIfNotEmpty();
+    this.content = _builder;
+  }
+  
+  /**
+   * @param context
+   * @param node
+   * @return
+   */
+  private String getCVariableDeclaration(final IGeneratorContext context, final ComponentNode node) {
+    final IComponentGenerator generator = InternalGeneratorUtil.getComponentGenerator(node);
+    IComponentGeneratorContext _context = generator.getContext();
+    final IComponentSignature signature = _context.getComponentSignature();
+    Component _component = node.getComponent();
+    final InputPort inputPort = _component.getFirstInputPort();
+    final DataType dataType = signature.getInputDataType(inputPort);
+    Configuration _configuration = context.getConfiguration();
+    ComputationModel _computationModel = GeneratorConfigurationUtil.getComputationModel(_configuration, node);
+    Component _component_1 = node.getComponent();
+    String _name = _component_1.getName();
+    String _uncapitalize = InternalGeneratorUtil.uncapitalize(_name);
+    return MscriptGeneratorUtil.getCVariableDeclaration(_computationModel, context, dataType, _uncapitalize, false, this);
+  }
+  
+  public CharSequence generateForwardDeclaration(final boolean internal) {
+    return this.content;
+  }
+}

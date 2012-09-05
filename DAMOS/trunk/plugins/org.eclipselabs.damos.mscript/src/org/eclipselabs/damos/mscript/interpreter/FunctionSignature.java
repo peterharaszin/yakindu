24d8271fc654116c97df3e4809cfe9705651bc5c
au@andreasunger.com <au@andreasunger.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-09-05 17:05:36 +0000

    FunctionSignature class changes.

--- DAMOS/trunk/plugins/org.eclipselabs.damos.mscript/src/org/eclipselabs/damos/mscript/interpreter/FunctionSignature.java
+++ DAMOS/trunk/plugins/org.eclipselabs.damos.mscript/src/org/eclipselabs/damos/mscript/interpreter/FunctionSignature.java
@@ -12,14 +12,8 @@
 package org.eclipselabs.damos.mscript.interpreter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
-import org.eclipselabs.damos.mscript.DataType;
-import org.eclipselabs.damos.mscript.Type;
-import org.eclipselabs.damos.mscript.interpreter.value.IBooleanValue;
-import org.eclipselabs.damos.mscript.interpreter.value.INumericValue;
-import org.eclipselabs.damos.mscript.interpreter.value.ISimpleNumericValue;
-import org.eclipselabs.damos.mscript.interpreter.value.IValue;
 
 /**
  * @author Andreas Unger
@@ -27,20 +21,18 @@ import org.eclipselabs.damos.mscript.interpreter.value.IValue;
  */
 public class FunctionSignature {
 
-	private List<ValueWrapper> staticParameterValues = new ArrayList<ValueWrapper>();
-	private List<TypeWrapper> parameterTypes = new ArrayList<TypeWrapper>();
+	private final List<FunctionParameter> parameters;
 	private int cachedHashCode;
 	
 	/**
 	 * 
 	 */
-	public FunctionSignature(List<IValue> staticParameterValues, List<DataType> parameterTypes) {
-		for (IValue value : staticParameterValues) {
-			this.staticParameterValues.add(new ValueWrapper(value));
-		}
-		for (Type type : parameterTypes) {
-			this.parameterTypes.add(new TypeWrapper(type));
-		}
+	private FunctionSignature(List<FunctionParameter> parameters) {
+		this.parameters = new ArrayList<FunctionParameter>(parameters);
+	}
+	
+	public static FunctionSignature create(List<FunctionParameter> parameters) {
+		return new FunctionSignature(Collections.unmodifiableList(new ArrayList<FunctionParameter>(parameters)));
 	}
 	
 	/* (non-Javadoc)
@@ -49,7 +41,7 @@ public class FunctionSignature {
 	@Override
 	public int hashCode() {
 		if (cachedHashCode == 0) {
-			cachedHashCode = 17 + staticParameterValues.hashCode() + parameterTypes.hashCode();
+			cachedHashCode = 17 + parameters.hashCode();
 			if (cachedHashCode == 0) { 
 				cachedHashCode = 1;
 			}
@@ -64,75 +56,9 @@ public class FunctionSignature {
 	public boolean equals(Object obj) {
 		if (obj instanceof FunctionSignature) {
 			FunctionSignature other = (FunctionSignature) obj;
-			return other.staticParameterValues.equals(staticParameterValues) && other.parameterTypes.equals(parameterTypes);
+			return other.parameters.equals(parameters);
 		}
 		return false;
 	}
-
-	private static class ValueWrapper {
 		
-		private final IValue value;
-		
-		public ValueWrapper(IValue value) {
-			this.value = value;
-		}
-		
-		/* (non-Javadoc)
-		 * @see java.lang.Object#hashCode()
-		 */
-		@Override
-		public int hashCode() {
-			int hashCode = ValueWrapper.class.hashCode();
-			if (value instanceof INumericValue) {
-				hashCode += Double.toString(((ISimpleNumericValue) value).doubleValue()).hashCode();
-			}
-			return hashCode;
-		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			if (obj instanceof ValueWrapper) {
-				ValueWrapper other = (ValueWrapper) obj;
-				IValue otherValue = other.value;
-				if (!otherValue.getDataType().isEquivalentTo(value.getDataType())) {
-					return false;
-				}
-				IValue result = otherValue.equalTo(value);
-				return result instanceof IBooleanValue && ((IBooleanValue) result).booleanValue();
-			}
-			return false;
-		}
-		
-	}
-
-	private static class TypeWrapper {
-	
-		private final Type dataType;
-
-		/**
-		 * @param dataType
-		 */
-		public TypeWrapper(Type dataType) {
-			this.dataType = dataType;
-		}
-		
-		/* (non-Javadoc)
-		 * @see java.lang.Object#hashCode()
-		 */
-		@Override
-		public int hashCode() {
-			return TypeWrapper.class.hashCode();
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (obj instanceof DataType) {
-				DataType other = (DataType) obj;
-				return other.isEquivalentTo(dataType);
-			}
-			return false;
-		}
-		
-	}
-	
 }

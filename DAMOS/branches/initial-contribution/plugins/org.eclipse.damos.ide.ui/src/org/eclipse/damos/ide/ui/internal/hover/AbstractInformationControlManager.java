5abf6f4097a2c6b3c02f5e4ba4b85433daeba2e2
au@andreasunger.com <au@andreasunger.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2012-11-05 19:59:39 +0000

    Copyright headers fixed.

--- DAMOS/branches/initial-contribution/plugins/org.eclipse.damos.ide.ui/src/org/eclipse/damos/ide/ui/internal/hover/AbstractInformationControlManager.java
+++ DAMOS/branches/initial-contribution/plugins/org.eclipse.damos.ide.ui/src/org/eclipse/damos/ide/ui/internal/hover/AbstractInformationControlManager.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 Andreas Unger and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Andreas Unger - initial API and implementation
+ *     IBM Corporation - initial API and implementation
+ *     Sean Montgomery, sean_montgomery@comcast.net - https://bugs.eclipse.org/bugs/show_bug.cgi?id=45095
  *******************************************************************************/
 package org.eclipse.damos.ide.ui.internal.hover;
 
--- DAMOS/branches/initial-contribution/plugins/org.eclipse.damos.mscript/src/org/eclipse/damos/mscript/function/impl/FunctionDescriptionBuilder.java
+++ DAMOS/branches/initial-contribution/plugins/org.eclipse.damos.mscript/src/org/eclipse/damos/mscript/function/impl/FunctionDescriptionBuilder.java
@@ -1,4 +1,4 @@
-/****************************************************************************
+/*******************************************************************************
  * Copyright (c) 2008, 2012 Andreas Unger and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -6,8 +6,8 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Andreas Unger - initial API and implementation 
- ****************************************************************************/
+ *     Andreas Unger - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.damos.mscript.function.impl;
 

05124c99bc2ff8ec262cc70db5bc28fd436bc8d5
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-02-08 15:12:50 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-1569.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-1569/plugins/org.yakindu.sct.refactoring/src/org/yakindu/sct/refactoring/refactor/impl/UnfoldExitActionsRefactoring.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2013 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.refactoring.refactor.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.yakindu.sct.model.sgraph.Effect;
+import org.yakindu.sct.model.sgraph.State;
+import org.yakindu.sct.model.sgraph.Transition;
+import org.yakindu.sct.model.stext.stext.Expression;
+import org.yakindu.sct.model.stext.stext.ReactionEffect;
+import org.yakindu.sct.model.stext.stext.StextFactory;
+import org.yakindu.sct.model.stext.stext.impl.ExitEventImpl;
+import org.yakindu.sct.refactoring.refactor.AbstractRefactoring;
+/**
+ * This refactoring moves exit actions of a state to its outgoing transitions.
+ * <br><br>
+ * Context:
+ * <ul>
+ * <li>Exactly one state</li>
+ * </ul>
+ * Preconditions:
+ * <ul>
+ * <li>At least one exit action exists.</li>
+ * <li>No outgoing transition leaves a parent composite state of the context state with exit actions.</li>
+ * </ul>
+ * 
+ * @author thomas kutz - Initial contribution and API
+ * 
+ */
+public class UnfoldExitActionsRefactoring extends AbstractRefactoring<State> {
+
+	@Override
+	protected void internalExecute() {
+		unfoldExitActions();
+	}
+
+	/**
+	 * Checks if one of the outgoing transitions leaves a parent composite state
+	 * of its source. If so, false is returned if this parent composite state
+	 * has exit actions.
+	 * 
+	 * @return true if all preconditions are fulfilled, false otherwise
+	 */
+	@Override
+	public boolean isExecutable() {
+		return super.isExecutable()
+				&& !helper.oneOutgoingTransitionLeavesCompositeWithExitActions(getContextObject());
+	}
+
+
+
+	private void unfoldExitActions() {
+		List<Expression> actionsToUnfold = new ArrayList<Expression>(
+				helper.extractAllLocalActionsForEventType(getContextObject(),
+						ExitEventImpl.class));
+		addActionsToOutgoingTransitions(actionsToUnfold);
+	}
+
+	private void addActionsToOutgoingTransitions(List<Expression> actionsToAdd) {
+		for (Transition transition : getContextObject()
+				.getOutgoingTransitions()) {
+			addActionsToTransition(transition, EcoreUtil.copyAll(actionsToAdd));
+		}
+	}
+
+	private void addActionsToTransition(final Transition transition,
+			final Collection<Expression> actionsToAdd) {
+
+		Effect effect = transition.getEffect();
+		if (effect instanceof ReactionEffect) {
+			ReactionEffect reactionEffect = (ReactionEffect) effect;
+			reactionEffect.getActions().addAll(0, actionsToAdd);
+		} else {
+			ReactionEffect reactionEffect = StextFactory.eINSTANCE
+					.createReactionEffect();
+			transition.setEffect(reactionEffect);
+			reactionEffect.getActions().addAll(actionsToAdd);
+		}
+	}
+
+	@Override
+	protected String getCommandLabel() {
+		return "Unfold Exit Actions";
+	}
+
+}
---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-1569/test-plugins/org.yakindu.sct.refactoring.tests/src/org/yakindu/sct/refactoring/refactor/RenameRefactoringTest.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright (c) 2013 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.refactoring.refactor;
+
+import static org.junit.Assert.fail;
+import static org.yakindu.sct.test.models.RefactoringTestModels.INITIAL_STATECHART;
+import static org.yakindu.sct.test.models.RefactoringTestModels.RENAMING;
+import junit.framework.Assert;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.eclipse.xtext.parser.IParser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.yakindu.sct.model.sgraph.Event;
+import org.yakindu.sct.model.sgraph.Scope;
+import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.model.sgraph.Variable;
+import org.yakindu.sct.model.sgraph.resource.AbstractSCTResource;
+import org.yakindu.sct.model.stext.stext.InterfaceScope;
+import org.yakindu.sct.refactoring.refactor.impl.RenameRefactoring;
+import org.yakindu.sct.refactoring.refactor.util.TestHelper;
+import org.yakindu.sct.refactoring.refactor.util.TestInjectorProvider;
+import org.yakindu.sct.test.models.RefactoringTestModels;
+
+import com.google.inject.Inject;
+
+/**
+ * Tests for {@link RenameRefactoring}.
+ * 
+ * @author thomas kutz - Initial contribution and API
+ * 
+ */
+@RunWith(XtextRunner.class)
+@InjectWith(TestInjectorProvider.class)
+public class RenameRefactoringTest {
+
+	@Inject
+	protected IParser parser;
+
+	@Inject
+	protected RefactoringTestModels models;
+
+	protected TestHelper helper = new TestHelper();
+
+	protected void compareStatecharts(Statechart initial, Statechart expected) {
+		if (!EcoreUtil.equals(initial, expected)) {
+			Assert.fail("Equality check on statecharts failed!");
+		}
+	}
+
+	@Test
+	public void testRenameVariable() {
+		Statechart initial = models.loadStatechartFromResource(RENAMING
+				+ INITIAL_STATECHART);
+
+		// get element to rename
+		Variable someVariable = null;
+		EList<Scope> scopes = initial.getScopes();
+		for (Scope scope : scopes) {
+			if (scope instanceof InterfaceScope) {
+				InterfaceScope iScope = (InterfaceScope) scope;
+				someVariable = iScope.getVariables().get(0);
+			}
+		}
+
+		AbstractRefactoring<?> refactoring = new RenameRefactoring(
+				someVariable, "someNewVariableName");
+
+		((AbstractSCTResource) initial.eResource()).setSerializerEnabled(true);
+		refactoring.internalExecute();
+		((AbstractSCTResource) initial.eResource()).setSerializerEnabled(false);
+
+		Statechart expected = models.loadStatechartFromResource(RENAMING
+				+ "AfterRenamingVariable.sct");
+
+		compareStatecharts(initial, expected);
+	}
+
+	@Test
+	public void testRenameEvent() {
+		Statechart initial = models.loadStatechartFromResource(RENAMING
+				+ INITIAL_STATECHART);
+
+		// get element to rename
+		Event someEvent1 = null;
+		EList<Scope> scopes = initial.getScopes();
+		for (Scope scope : scopes) {
+			if (scope instanceof InterfaceScope) {
+				InterfaceScope iScope = (InterfaceScope) scope;
+				for (Event event : iScope.getEvents()) {
+					if (event.getName().equals("someEvent1")) {
+						someEvent1 = event;
+					}
+				}
+			}
+		}
+
+		AbstractRefactoring<?> refactoring = new RenameRefactoring(someEvent1,
+				"someNewEventName");
+
+		((AbstractSCTResource) initial.eResource()).setSerializerEnabled(true);
+		refactoring.internalExecute();
+		((AbstractSCTResource) initial.eResource()).setSerializerEnabled(false);
+
+		Statechart expected = models.loadStatechartFromResource(RENAMING
+				+ "AfterRenamingEvent.sct");
+
+		compareStatecharts(initial, expected);
+	}
+
+	@Test
+	public void testRenameInterface() {
+		Statechart initial = models.loadStatechartFromResource(RENAMING
+				+ INITIAL_STATECHART);
+
+		// get element to rename
+		InterfaceScope someInterface = null;
+		EList<Scope> scopes = initial.getScopes();
+		for (Scope scope : scopes) {
+			if (scope instanceof InterfaceScope) {
+				someInterface = (InterfaceScope) scope;
+			}
+		}
+
+		AbstractRefactoring<?> refactoring = new RenameRefactoring(
+				someInterface, "someNewInterfaceName");
+
+		((AbstractSCTResource) initial.eResource()).setSerializerEnabled(true);
+		refactoring.internalExecute();
+		((AbstractSCTResource) initial.eResource()).setSerializerEnabled(false);
+
+		Statechart expected = models.loadStatechartFromResource(RENAMING
+				+ "AfterRenamingInterface.sct");
+
+		compareStatecharts(initial, expected);
+	}
+
+	@Test
+	public void testNoRenamingIntoExistingName() {
+		fail("Not yet implemented.");
+	}
+
+}

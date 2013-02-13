951293ac88e66291a008bb048a633384d60ed742
alexander.nyssen@itemis.de <alexander.nyssen@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-02-13 08:28:19 +0000

    Create tag on trunk to capture state before merging the TYPE_SYSTEM_REFACTORING branch.

---
+++ SCT2/tags/PRE_MERGE_TYPE_SYSTEM_REFACTORING/plugins/org.yakindu.sct.refactoring/src/org/yakindu/sct/refactoring/handlers/impl/InlineSubdiagramHandler.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2013 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.refactoring.handlers.impl;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.yakindu.sct.refactoring.handlers.AbstractViewRefactoringHandler;
+import org.yakindu.sct.refactoring.refactor.AbstractRefactoring;
+import org.yakindu.sct.refactoring.refactor.impl.InlineSubdiagramRefactoring;
+import org.yakindu.sct.ui.editor.breadcrumb.DiagramPartitioningUtil;
+import org.yakindu.sct.ui.editor.editparts.StateEditPart;
+
+/**
+ * Handler for {@link InlineSubdiagramRefactoring}.
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+public class InlineSubdiagramHandler extends AbstractViewRefactoringHandler {
+
+	@Override
+	public AbstractRefactoring<View> createRefactoring() {
+		return new InlineSubdiagramRefactoring();
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		if (allEditorsAreClosed(event))
+			return super.execute(event);
+		return null;
+	}
+
+	private boolean allEditorsAreClosed(ExecutionEvent event) {
+		StateEditPart firstElement = (StateEditPart) getFirstElement(HandlerUtil.getCurrentSelection(event));
+		return DiagramPartitioningUtil.closeSubdiagramEditors(firstElement.resolveSemanticElement());
+	}
+}

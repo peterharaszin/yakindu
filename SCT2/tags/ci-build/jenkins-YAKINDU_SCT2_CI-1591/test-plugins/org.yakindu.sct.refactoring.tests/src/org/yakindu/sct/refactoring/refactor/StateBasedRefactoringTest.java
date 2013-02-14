847ba90b13e7d48db76885dc050fc282962ec760
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-02-14 20:18:56 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-1591.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-1591/test-plugins/org.yakindu.sct.refactoring.tests/src/org/yakindu/sct/refactoring/refactor/StateBasedRefactoringTest.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2013 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.refactoring.refactor;
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipse.xtext.junit4.XtextRunner;
+import org.junit.runner.RunWith;
+import org.yakindu.sct.model.sgraph.State;
+import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.model.sgraph.resource.AbstractSCTResource;
+import org.yakindu.sct.test.models.TestModelInjectorProvider;
+
+/**
+ * Base test class for state based refactorings.
+ * 
+ * @author thomas kutz - Initial contribution and API
+ * 
+ */
+@RunWith(XtextRunner.class)
+@InjectWith(TestModelInjectorProvider.class)
+public abstract class StateBasedRefactoringTest extends RefactoringTest {
+
+	protected void testRefactoringOnState(String pathToInitialSct,
+			String pathToExpectedSct, String stateName) {
+		Statechart initial = models
+				.loadStatechartFromResource(pathToInitialSct);
+
+		State state = getStateByName(initial, stateName);
+
+		AbstractRefactoring<?> refactoring = getRefactoring(state);
+		((AbstractSCTResource) initial.eResource()).setSerializerEnabled(true);
+		refactoring.internalExecute();
+		((AbstractSCTResource) initial.eResource()).setSerializerEnabled(false);
+
+		Statechart expected = models
+				.loadStatechartFromResource(pathToExpectedSct);
+		
+		parseAllSpecifications(initial);
+		parseAllSpecifications(expected);
+		
+		compareStatecharts(initial, expected);
+	}
+	
+	protected void testRefactoringIsExecutableOnState(String pathToInitialSct,
+			String pathToExpectedSct, String stateName, boolean expectedResult) {
+		Statechart initial = models
+				.loadStatechartFromResource(pathToInitialSct);
+
+		State state = getStateByName(initial, stateName);
+
+		AbstractRefactoring<?> refactoring = getRefactoring(state);
+		if (expectedResult) {
+			Assert.assertTrue("Refactoring on state '"+stateName+"' was not executable, although it should be.", refactoring.isExecutable());
+		}
+		else {			
+			Assert.assertFalse("Refactoring on state '"+stateName+"' was executable, although it should not be.", refactoring.isExecutable());
+		}
+	}
+
+	protected abstract AbstractRefactoring<?> getRefactoring(State state);
+	
+	protected State getStateByName(Statechart statechart, String name) {
+		List<State> allStates = EcoreUtil2.getAllContentsOfType(statechart, State.class);
+		for (State state : allStates) {
+			if (state.getName().equals(name)) {
+				return state;
+			}
+		}
+		return null;
+	}
+}

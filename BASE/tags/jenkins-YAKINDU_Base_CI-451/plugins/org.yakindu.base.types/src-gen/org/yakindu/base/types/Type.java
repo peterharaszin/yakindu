178ecbbf18ea16e3b42520d04dd5e022e99f6cb0
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-02-22 09:04:13 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_Base_CI-451.

---
+++ BASE/tags/jenkins-YAKINDU_Base_CI-451/plugins/org.yakindu.base.types/src-gen/org/yakindu/base/types/Type.java
@@ -0,0 +1,46 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.yakindu.base.types;
+
+import org.eclipse.emf.common.util.EList;
+import org.yakindu.base.base.NamedElement;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Type</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.yakindu.base.types.Type#getConstraint <em>Constraint</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.yakindu.base.types.TypesPackage#getType()
+ * @model abstract="true"
+ * @generated
+ */
+public interface Type extends NamedElement {
+
+	/**
+	 * Returns the value of the '<em><b>Constraint</b></em>' containment reference list.
+	 * The list contents are of type {@link org.yakindu.base.types.TypeConstraint}.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Constraint</em>' containment reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Constraint</em>' containment reference list.
+	 * @see org.yakindu.base.types.TypesPackage#getType_Constraint()
+	 * @model containment="true"
+	 * @generated
+	 */
+	EList<TypeConstraint> getConstraint();
+
+} // Type

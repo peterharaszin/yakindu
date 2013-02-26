1b92721e9f861b1631b8f62b60ff6f16daded211
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-02-26 16:13:21 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-1626.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-1626/plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/editparts/RegionCompartmentEditPart.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2010 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.ui.editor.editparts;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.LayoutAnimator;
+import org.eclipse.gef.EditPolicy;
+import org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeCompartmentEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.DragDropEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
+import org.eclipse.gmf.runtime.diagram.ui.figures.ResizableCompartmentFigure;
+import org.eclipse.gmf.runtime.diagram.ui.figures.ShapeCompartmentFigure;
+import org.eclipse.gmf.runtime.notation.View;
+import org.yakindu.sct.ui.editor.DiagramActivator;
+import org.yakindu.sct.ui.editor.policies.CompartmentCreationEditPolicy;
+import org.yakindu.sct.ui.editor.policies.RegionCompartmentCanonicalEditPolicy;
+import org.yakindu.sct.ui.editor.policies.RegionCompartmentXYLayoutEditPolicy;
+import org.yakindu.sct.ui.editor.policies.ResizeContainerEditPolicy;
+
+/**
+ * @author muelder
+ * @author muehlbrandt
+ */
+public class RegionCompartmentEditPart extends ShapeCompartmentEditPart {
+
+	private boolean isSupportingViewActions = true;
+
+	public RegionCompartmentEditPart(View view) {
+		super(view);
+	}
+
+	@Override
+	protected void createDefaultEditPolicies() {
+		super.createDefaultEditPolicies();
+		installEditPolicy(EditPolicyRoles.CREATION_ROLE, new CompartmentCreationEditPolicy());
+		installEditPolicy(EditPolicyRoles.CANONICAL_ROLE, new RegionCompartmentCanonicalEditPolicy());
+		installEditPolicy(EditPolicyRoles.DRAG_DROP_ROLE, new DragDropEditPolicy());
+		installEditPolicy(EditPolicy.LAYOUT_ROLE, new RegionCompartmentXYLayoutEditPolicy());
+		installEditPolicy(EditPolicy.PRIMARY_DRAG_ROLE, new ResizeContainerEditPolicy());
+	}
+
+	@Override
+	public PreferencesHint getDiagramPreferencesHint() {
+		return DiagramActivator.DIAGRAM_PREFERENCES_HINT;
+	}
+
+	public boolean isSupportingViewActions() {
+		return this.isSupportingViewActions;
+	}
+
+	public void setIsSupportingViewActions(boolean supportsViewActions) {
+		this.isSupportingViewActions = supportsViewActions;
+	}
+
+	@Override
+	protected IFigure createFigure() {
+		ShapeCompartmentFigure figure = new ShapeCompartmentFigure(getCompartmentName(), getMapMode());
+		figure.getContentPane().setLayoutManager(getLayoutManager());
+		figure.getContentPane().addLayoutListener(LayoutAnimator.getDefault());
+		figure.setBorder(null);
+		//
+		figure.setToolTip((String) null);
+		return figure;
+	}
+
+	@Override
+	public boolean isSelectable() {
+		return false;
+	}
+
+	@Override
+	protected void refreshVisuals() {
+		super.refreshVisuals();
+		((ResizableCompartmentFigure) getFigure()).getScrollPane().setScrollBarVisibility(
+				org.eclipse.draw2d.ScrollPane.NEVER);
+	}
+
+	@Override
+	protected void refreshConnections() {
+		// Do nothing to prevent super.refreshConnections() from clearing away
+		// connections which are now clipped by ViewportAwareClippingStrategy.
+	}
+}

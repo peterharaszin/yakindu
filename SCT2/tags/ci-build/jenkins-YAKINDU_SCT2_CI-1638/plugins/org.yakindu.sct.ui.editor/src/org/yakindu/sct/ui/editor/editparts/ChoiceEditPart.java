963d80a87b10839beaa27c3dac5d35d6cecf242e
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-03-05 13:17:34 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-1638.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-1638/plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/editparts/ChoiceEditPart.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2010 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.ui.editor.editparts;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.gmf.runtime.notation.View;
+import org.yakindu.sct.ui.editor.editor.figures.ChoiceFigure;
+import org.yakindu.sct.ui.editor.editor.figures.utils.MapModeUtils;
+import org.yakindu.sct.ui.editor.policies.EnlargeContainerEditPart;
+
+import de.itemis.gmf.runtime.commons.editparts.FixedSizeShapeNodeEditPart;
+
+/**
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+public class ChoiceEditPart extends FixedSizeShapeNodeEditPart {
+
+	public ChoiceEditPart(View view) {
+		super(view);
+	}
+
+	@Override
+	protected void createDefaultEditPolicies() {
+		super.createDefaultEditPolicies();
+		installEditPolicy(EnlargeContainerEditPart.ROLE, new EnlargeContainerEditPart());
+	}
+
+	@Override
+	public Dimension getDefaultSize() {
+		return MapModeUtils.getMappedDimensions(getMapMode(), MapModeUtils.DEFAULT_SMALL_NODE_DIMENSION);
+	}
+
+	@Override
+	public IFigure getPrimaryShape() {
+		return new ChoiceFigure();
+	}
+
+}

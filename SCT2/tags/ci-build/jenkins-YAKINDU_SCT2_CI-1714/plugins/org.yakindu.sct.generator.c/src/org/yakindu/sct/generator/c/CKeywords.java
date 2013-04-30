f944cc9c5e233fd33d87bf0e4b2b506388d42112
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-04-30 13:45:12 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-1714.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-1714/plugins/org.yakindu.sct.generator.c/src/org/yakindu/sct/generator/c/CKeywords.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2012 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.generator.c;
+
+public interface CKeywords {
+	public static final String[] KEYWORDS = { "alignas", "alignof", "and",
+			"and_eq", "asm", "auto", "bitand", "bitor", "bool", "break",
+			"case", "catch", "char", "char16_t", "char32_t", "class", "compl",
+			"const", "constexpr", "const_cast", "continue", "decltype",
+			"default", "delete", "do", "double", "dynamic_cast", "else",
+			"enum", "explicit", "export", "extern", "false", "float", "for",
+			"friend", "goto", "if", "inline", "int", "long", "mutable",
+			"namespace", "new", "noexcept", "not", "not_eq", "nullptr",
+			"operator", "or", "or_eq", "private", "protected", "public",
+			"register", "reinterpret_cast", "return", "short", "signed",
+			"sizeof", "static", "static_assert", "static_cast", "struct",
+			"switch", "template", "this", "thread_local", "throw", "true",
+			"try", "typedef", "typeid", "typename", "union", "unsigned",
+			"using", "virtual", "void", "volatile", "wchar_t", "while", "xor",
+			"xor_eq" };
+}

d26e18c79c1352746cc63c12a8b301b81eab99d6
A.Muelder@gmail.com <A.Muelder@gmail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2013-11-28 15:40:15 +0000

    Removed Damos, part of eclipse

--- DAMOS/branches/0.3.0_RELEASE/plugins/org.eclipselabs.damos.library.base/src/org/eclipselabs/damos/library/base/signaturepolicies/sinks/XYScopeSignaturePolicy.java
+++
@@ -1,9 +0,0 @@
-package org.eclipselabs.damos.library.base.signaturepolicies.sinks;
-
-/**
- * @author Andreas Unger
- *
- */
-public class XYScopeSignaturePolicy extends AbstractScopeSignaturePolicy {
-	
-}

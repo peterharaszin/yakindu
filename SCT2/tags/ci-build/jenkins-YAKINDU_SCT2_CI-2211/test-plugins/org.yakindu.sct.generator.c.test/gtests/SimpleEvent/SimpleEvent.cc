0d601751caff505d156df39f66c0b4bd3adae50d
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-05-15 12:24:20 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-2211.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-2211/test-plugins/org.yakindu.sct.generator.c.test/gtests/SimpleEvent/SimpleEvent.cc
@@ -0,0 +1,30 @@
+/**
+* Copyright (c) 2014 committers of YAKINDU and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     committers of YAKINDU - initial API and implementation
+*/
+#include <string>
+#include "gtest/gtest.h"
+#include "SimpleEvent.h"
+
+
+SimpleEvent handle;
+
+TEST(StatemachineTest, simpleEventTest) {
+	simpleEvent_init(&handle);
+	simpleEvent_enter(&handle);
+	EXPECT_TRUE(simpleEvent_isActive(&handle, SimpleEvent_A)) << "Expected A to be active" ;
+	EXPECT_TRUE(5== 5);
+	simpleEventIface_raise_event1(&handle);
+	simpleEvent_runCycle(&handle);
+	EXPECT_TRUE(simpleEvent_isActive(&handle, SimpleEvent_B)) << "Expected B to be active" ;
+	simpleEvent_runCycle(&handle);
+	EXPECT_TRUE(!simpleEvent_isActive(&handle, SimpleEvent_B));
+}
+
+		

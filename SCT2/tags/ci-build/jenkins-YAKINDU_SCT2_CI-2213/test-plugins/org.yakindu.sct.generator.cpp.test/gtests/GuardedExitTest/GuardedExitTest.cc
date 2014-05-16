b1a89f5992082eb56e73754c4d3727a75f0879a6
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-05-16 12:55:21 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-2213.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-2213/test-plugins/org.yakindu.sct.generator.cpp.test/gtests/GuardedExitTest/GuardedExitTest.cc
@@ -0,0 +1,38 @@
+/**
+* Copyright (c) 2014 committers of YAKINDU and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     committers of YAKINDU - initial API and implementation
+*/
+#include <string>
+#include "gtest/gtest.h"
+#include "GuardedExit.h"
+
+TEST(StatemachineTest, ExitTaken) {
+	GuardedExit* statechart = new GuardedExit();
+	statechart->init();
+	statechart->enter();
+	EXPECT_TRUE(statechart->isActive(GuardedExit::A));
+	EXPECT_TRUE(!statechart->getSCInterface()->get_guard());
+	statechart->raise_e();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(GuardedExit::B));
+	EXPECT_TRUE(!statechart->getSCInterface()->get_done());
+	delete statechart;
+}
+TEST(StatemachineTest, ExitNotTaken) {
+	GuardedExit* statechart = new GuardedExit();
+	statechart->init();
+	statechart->enter();
+	EXPECT_TRUE(statechart->isActive(GuardedExit::A));
+	statechart->getSCInterface()->set_guard(true);
+	statechart->raise_e();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(GuardedExit::B));
+	EXPECT_TRUE(statechart->getSCInterface()->get_done());
+	delete statechart;
+}

50cb1b4381cdca2cfe4430fa0ececcde2d3eac8c
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-05-26 10:02:18 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-2216.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-2216/test-plugins/org.yakindu.sct.generator.cpp.test/gtests/HistoryWithoutInitialStepTest/HistoryWithoutInitialStepTest.cc
@@ -0,0 +1,61 @@
+/**
+* Copyright (c) 2014 committers of YAKINDU and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     committers of YAKINDU - initial API and implementation
+*/
+#include <string>
+#include "gtest/gtest.h"
+#include "HistoryWithoutInitialStep.h"
+
+TEST(StatemachineTest, enterThroughInitialEntry) {
+	HistoryWithoutInitialStep* statechart = new HistoryWithoutInitialStep();
+	statechart->init();
+	statechart->enter();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::A));
+	statechart->raise_e1();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::C));
+	statechart->raise_e2();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::D));
+	delete statechart;
+}
+TEST(StatemachineTest, enterCThroughHistory) {
+	HistoryWithoutInitialStep* statechart = new HistoryWithoutInitialStep();
+	statechart->init();
+	statechart->enter();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::A));
+	statechart->raise_e1();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::C));
+	statechart->raise_e1();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::A));
+	statechart->raise_e2();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::C));
+	delete statechart;
+}
+TEST(StatemachineTest, enterDThroughHistory) {
+	HistoryWithoutInitialStep* statechart = new HistoryWithoutInitialStep();
+	statechart->init();
+	statechart->enter();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::A));
+	statechart->raise_e1();
+	statechart->runCycle();
+	statechart->raise_e2();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::D));
+	statechart->raise_e1();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::A));
+	statechart->raise_e2();
+	statechart->runCycle();
+	EXPECT_TRUE(statechart->isActive(HistoryWithoutInitialStep::D));
+	delete statechart;
+}

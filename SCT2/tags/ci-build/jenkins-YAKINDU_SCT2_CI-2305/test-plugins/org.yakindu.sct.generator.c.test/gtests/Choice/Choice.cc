4186de464b38e1404f1e69cf853b2bda5318bb5b
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-07-28 14:40:28 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-2305.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-2305/test-plugins/org.yakindu.sct.generator.c.test/gtests/Choice/Choice.cc
@@ -0,0 +1,82 @@
+/**
+* Copyright (c) 2014 committers of YAKINDU and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     committers of YAKINDU - initial API and implementation
+*/
+#include <string>
+#include "gtest/gtest.h"
+#include "Choice.h"
+
+
+Choice handle;
+
+TEST(StatemachineTest, elseChoiceUsingNonDefaultTransition) {
+	choice_init(&handle);
+	choice_enter(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_A));
+	choiceIface_set_c(&handle, true);
+	choiceIface_raise_e(&handle);
+	choice_runCycle(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_C));
+}
+TEST(StatemachineTest, elseChoiceUsingDefaultTransition) {
+	choice_init(&handle);
+	choice_enter(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_A));
+	choiceIface_set_c(&handle, false);
+	choiceIface_raise_e(&handle);
+	choice_runCycle(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_B));
+}
+TEST(StatemachineTest, defaultChoiceUsingNonDefaultTransition) {
+	choice_init(&handle);
+	choice_enter(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_A));
+	choiceIface_set_c(&handle, true);
+	choiceIface_raise_g(&handle);
+	choice_runCycle(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_C));
+}
+TEST(StatemachineTest, defaultChoiceUsingDefaultTransition) {
+	choice_init(&handle);
+	choice_enter(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_A));
+	choiceIface_set_c(&handle, false);
+	choiceIface_raise_g(&handle);
+	choice_runCycle(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_B));
+}
+TEST(StatemachineTest, uncheckedChoiceUsingNonDefaultTransition) {
+	choice_init(&handle);
+	choice_enter(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_A));
+	choiceIface_set_c(&handle, true);
+	choiceIface_raise_f(&handle);
+	choice_runCycle(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_C));
+}
+TEST(StatemachineTest, uncheckedChoiceUsingDefaultTransition) {
+	choice_init(&handle);
+	choice_enter(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_A));
+	choiceIface_set_c(&handle, false);
+	choiceIface_raise_f(&handle);
+	choice_runCycle(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_B));
+}
+TEST(StatemachineTest, alwaysTrueTransitionInChoice) {
+	choice_init(&handle);
+	choice_enter(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_A));
+	choiceIface_set_c(&handle, true);
+	choiceIface_raise_h(&handle);
+	choice_runCycle(&handle);
+	EXPECT_TRUE(choice_isActive(&handle, Choice_C));
+}
+
+		

f93a5be8e402bb374741d0ac6113e91faf1ad70e
A.Muelder@googlemail.com <A.Muelder@googlemail.com@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2014-11-27 14:14:32 +0000

    removed to recommit

--- SCT2/branches/Bugfix_2.4.x/plugins/org.yakindu.sct.generator.core/src/org/yakindu/sct/generator/core/types/ICodegenTypeSystemAccess.java
+++
@@ -1,8 +0,0 @@
-package org.yakindu.sct.generator.core.types;
-
-import org.yakindu.base.types.Type;
-
-public interface ICodegenTypeSystemAccess {
-
-	public String getTargetLanguageName(Type type);
-}
--- SCT2/branches/Bugfix_2.4.x/plugins/org.yakindu.sct.generator.cpp/src/org/yakindu/sct/generator/cpp/CppKeywords.java
+++
@@ -1,8 +0,0 @@
-package org.yakindu.sct.generator.cpp;
-
-
-public final class CppKeywords {
-	
-	public static final String[] CPP_KEYWORDS = { "clearInEvents", "clearOutEvents", "enter",
-		"exit", "runCycle"};
-}

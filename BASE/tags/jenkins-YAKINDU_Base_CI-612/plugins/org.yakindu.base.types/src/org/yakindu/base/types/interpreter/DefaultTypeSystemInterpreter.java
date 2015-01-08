b6bb92677e6abdc2860df54088a6a4ea3deaf46b
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2015-01-08 10:20:47 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_Base_CI-612.

---
+++ BASE/tags/jenkins-YAKINDU_Base_CI-612/plugins/org.yakindu.base.types/src/org/yakindu/base/types/interpreter/DefaultTypeSystemInterpreter.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2014 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.base.types.interpreter;
+
+import org.eclipse.emf.ecore.EObject;
+import org.yakindu.base.types.ITypeSystemRegistry;
+import org.yakindu.base.types.Type;
+import org.yakindu.base.types.typesystem.DefaultTypeSystem;
+import org.yakindu.base.types.typesystem.ITypeSystem;
+
+import com.google.inject.Inject;
+
+/**
+ * @author andreas muelder - Initial contribution and API
+ * 
+ */
+public class DefaultTypeSystemInterpreter extends AbstractTypeSystemInterpreter {
+
+	@Inject
+	private ITypeSystemRegistry typeSystem;
+
+	@Override
+	public Object interpret(EObject object) {
+		throw new IllegalArgumentException("not yet implemented");
+	}
+
+}

91a76db5fa56e0b9e073de5716c89ed281326f36
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2015-04-21 08:32:30 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-2592 on trunk.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-2592/plugins/org.yakindu.sct.simulation.core.sexec/src/org/yakindu/sct/simulation/core/sexec/launch/SexecLaunchConfigurationDelegate.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2011-2013 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * 	committers of YAKINDU - initial API and implementation
+ * 
+ */
+package org.yakindu.sct.simulation.core.sexec.launch;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.yakindu.sct.domain.extension.DomainRegistry;
+import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.simulation.core.engine.ISimulationEngine;
+import org.yakindu.sct.simulation.core.launch.AbstractSCTLaunchConfigurationDelegate;
+import org.yakindu.sct.simulation.core.sexec.container.ISimulationEngineFactory;
+
+import com.google.inject.Binder;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+
+/**
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * @author terfloth - added dynamic module configuration based on statechart
+ *         language type and language providers
+ * 
+ */
+public class SexecLaunchConfigurationDelegate extends AbstractSCTLaunchConfigurationDelegate implements
+		ILaunchConfigurationDelegate {
+
+	@Inject
+	private ISimulationEngineFactory factory;
+
+	@Override
+	protected ISimulationEngine createExecutionContainer(final ILaunch launch, Statechart statechart) {
+		Injector injector = DomainRegistry.getDomainDescriptor(statechart).getDomainInjectorProvider()
+				.getSimulationInjector();
+		Module module = new Module() {
+			@Override
+			public void configure(Binder binder) {
+				binder.bind(ILaunch.class).toInstance(launch);
+			}
+		};
+		injector.createChildInjector(module).injectMembers(this);
+		try {
+			return factory.createExecutionContainer(statechart, launch);
+		} catch (CoreException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}

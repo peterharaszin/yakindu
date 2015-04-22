644098855dd19b505e00ce344254d3a07a42fd1e
yakindu@itemis.de <yakindu@itemis.de@6a4fa09a-a2c1-c95f-7b62-f4ef5be68d87>
2015-04-22 10:24:48 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-2595 on trunk.

---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-2595/plugins/org.yakindu.sct.model.sexec/src/org/yakindu/sct/model/sexec/transformation/StextExtensions.xtend
@@ -0,0 +1,43 @@
+package org.yakindu.sct.model.sexec.transformation
+
+import org.yakindu.base.expressions.expressions.Expression
+import org.yakindu.base.expressions.expressions.ExpressionsFactory
+import org.yakindu.sct.model.sgraph.State
+import org.yakindu.sct.model.stext.stext.StextFactory
+
+/**
+ * @author Axel Terfloth added 'active' extension
+ */
+class StextExtensions {
+	
+	def expfactory() { ExpressionsFactory::eINSTANCE }
+	def factory() { StextFactory::eINSTANCE }
+
+	def Expression or(Expression left, Expression right) {
+		val or = expfactory.createLogicalOrExpression
+		or.leftOperand = left
+		or.rightOperand = right
+		or
+	}
+	
+	def Expression and(Expression left, Expression right) {
+		val and = expfactory.createLogicalAndExpression
+		and.leftOperand = left
+		and.rightOperand = right
+		and
+	}
+
+	def Expression parenthesis(Expression exp) {
+		val p = expfactory.createParenthesizedExpression
+		p.expression = exp
+		p
+	}
+
+
+	def Expression active(State state) {
+		val active = factory.createActiveStateReferenceExpression
+		active.value = state
+		active
+	}
+
+} 
---
+++ SCT2/tags/ci-build/jenkins-YAKINDU_SCT2_CI-2595/test-plugins/org.yakindu.sct.model.sexec.test/src/org/yakindu/sct/model/sexec/transformation/test/ModelSequencerTest.java
@@ -0,0 +1,46 @@
+package org.yakindu.sct.model.sexec.transformation.test;
+
+import org.junit.Before;
+import org.yakindu.sct.domain.generic.modules.GenericSequencerModule;
+import org.yakindu.sct.model.sexec.naming.DefaultNamingService;
+import org.yakindu.sct.model.sexec.naming.INamingService;
+import org.yakindu.sct.model.sexec.transformation.BehaviorMapping;
+import org.yakindu.sct.model.sexec.transformation.IModelSequencer;
+import org.yakindu.sct.model.sexec.transformation.StructureMapping;
+
+import com.google.inject.Binder;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.util.Modules;
+
+/**
+ * 
+ * @author axel terfloth
+ * 
+ */
+public class ModelSequencerTest extends Assert {
+
+	@Inject
+	protected IModelSequencer sequencer;
+
+	@Inject
+	protected BehaviorMapping behaviorMapping;
+
+	@Inject
+	protected StructureMapping structureMapping;
+
+	@Before
+	public void setup() {
+		Module m = Modules.override(new GenericSequencerModule()).with(new Module(){
+			@Override
+			public void configure(Binder binder) {
+				binder.bind(INamingService.class).to(DefaultNamingService.class);
+			}
+		});
+		Injector injector = Guice.createInjector(m);
+		injector.injectMembers(this);
+	}
+
+}
